f322dbde5aec89a6db049cc382e0b02dbe405470
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-05-29 00:37:47 -0300

    Update footer message

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,15 +5,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      © {{ year }} Nortan Projetos. Template criado com ❤️ por
+      <a
+        href="https://akveo.page.link/8V2f"
+        target="_blank"
+        style="text-decoration: none;"
+        >Akveo</a
+      >.
     </span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a
+        href="https://www.instagram.com/nortanprojetos/"
+        target="_blank"
+        class="ion ion-social-instagram"
+      ></a>
+      <a
+        href="https://www.linkedin.com/company/nortan-solução-integrada-em-projetos/"
+        target="_blank"
+        class="ion ion-social-linkedin"
+      ></a>
+      <a
+        href="https://github.com/nortan-projetos"
+        target="_blank"
+        class="ion ion-social-github"
+      ></a>
     </div>
   `,
 })
 export class FooterComponent {
+  year = new Date().getFullYear();
 }
