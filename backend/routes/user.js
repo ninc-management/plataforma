b38d42300b57d1ae95aebd4fc77b893780807bc8
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-06-30 12:06:04 -0300

    Improve backend get user logic

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,15 +5,13 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  let fetchedUser;
   User.findOne({ email: req.body.email }).then((user) => {
     if (!user) {
       return res.status(401).json({
         message: 'Email não cadastrado.',
       });
     }
-    fetchedUser = user;
-    return res.status(200).json(fetchedUser.toJSON());
+    return res.status(200).json(user.toJSON());
   });
 });
 
