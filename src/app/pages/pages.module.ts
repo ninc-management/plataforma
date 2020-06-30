2a06c1d6cb1054e74128478d35480a2bfe25d154
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-06-30 12:09:34 -0300

    Show logged user profile information

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { FormsModule } from '@angular/forms';
+import {
+  NbMenuModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbSelectModule,
+  NbInputModule,
+  NbButtonModule,
+} from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ProfileComponent } from './profile/profile.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
-    ThemeModule,
+    NbCardModule,
+    NbCheckboxModule,
+    NbSelectModule,
+    NbButtonModule,
     NbMenuModule,
+    NbInputModule,
+    ThemeModule,
     DashboardModule,
+    FormsModule,
+    SharedModule,
   ],
-  declarations: [
-    PagesComponent,
-    ProfileComponent,
-  ],
+  declarations: [PagesComponent, ProfileComponent],
 })
-export class PagesModule {
-}
+export class PagesModule {}
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NbAuthService } from '@nebular/auth';
+import { UserService } from '../../shared/services/user.service';
+import { StatecityService } from '../../shared/services/statecity.service';
+import * as user_validation from '../../shared/user-validation.json';
 
 @Component({
   selector: 'ngx-profile',
@@ -6,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  currentUser: any;
+  cities: string[] = [];
+  states: string[] = [];
+  validation = (user_validation as any).default;
 
-  ngOnInit(): void {}
+  constructor(
+    private authService: NbAuthService,
+    private userService: UserService,
+    private statecityService: StatecityService
+  ) {}
+
+  ngOnInit(): void {
+    this.states = this.statecityService.buildStateList();
+    this.authService.getToken().subscribe((token) => {
+      this.userService
+        .getUser(token.getPayload()['email'])
+        .subscribe((user) => {
+          this.currentUser = user;
+          this.cities = this.statecityService.buildCityList(
+            this.currentUser.state
+          );
+        });
+    });
+  }
+
+  buildCityList(state: string): void {
+    this.cities = this.statecityService.buildCityList(state);
+  }
 }
