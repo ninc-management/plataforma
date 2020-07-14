b1b0a78260c8f98b29767b98fb4470dbae3b5a6c
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-07-14 12:05:53 -0300

    Add register contract dialog

--- src/app/pages/contracts/contracts.component.ts
+++ src/app/pages/contracts/contracts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
+import { ContractDialogComponent } from './contract-dialog/contract-dialog.component';
 
 @Component({
   selector: 'ngx-contracts',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contracts.component.scss'],
 })
 export class ContractsComponent implements OnInit {
-  constructor() {}
+  constructor(private dialogService: NbDialogService) {}
 
   ngOnInit(): void {}
+
+  contractDialog(): void {
+    this.dialogService.open(ContractDialogComponent, {
+      context: {
+        title: 'Cadastro de contrato',
+      },
+      dialogClass: 'my-dialog',
+      closeOnBackdropClick: true,
+      closeOnEsc: true,
+    });
+  }
 }
