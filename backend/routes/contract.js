366fcae559dc0b5d2a0d2fb7c94dd69191ce93b7
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-07-15 10:37:23 -0300

    Create save contract endpoint in backend

--- backend/routes/contract.js
+++ backend/routes/contract.js
@@ -1,18 +1,23 @@
 const express = require('express');
 
-// const Contract = require('../models/contract');
+const Contract = require('../models/contract');
 
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  // User.findOne({ email: req.body.email }).then((user) => {
-  //   if (!user) {
-  //     return res.status(401).json({
-  //       message: 'Email não cadastrado.',
-  //     });
-  //   }
-  //   return res.status(200).json(user.toJSON());
-  // });
+  const contract = new Contract(req.body.contract);
+  contract
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: 'Contrato cadastrado!',
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
