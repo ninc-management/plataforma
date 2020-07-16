088930f87528290af4760708c26350263bcf2cf0
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-07-16 10:47:19 -0300

    Add contract form

--- backend/models/contract.js
+++ backend/models/contract.js
@@ -5,12 +5,12 @@ const contractSchema = mongoose.Schema({
   author: { type: mongoose.ObjectId, ref: 'User', required: true },
   department: { type: String, required: true },
   coordination: { type: String, required: true },
-  code: { type: String, required: true },
+  code: { type: String, required: true, unique: true },
   type: { type: String, required: true },
   service: { type: String, required: true },
   contractor: { type: String, required: true },
   name: { type: String, required: true },
-  value: { type: Number, required: true },
+  value: { type: String, required: true },
   trello: { type: Boolean, required: true },
 });
 
--- src/app/pages/contracts/contract-item/contract-item.component.ts
+++ src/app/pages/contracts/contract-item/contract-item.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import * as contract_validation from '../../../shared/contract-validation.json';
+import { ContractService } from '../../../shared/services/contract.service';
 
 @Component({
   selector: 'ngx-contract-item',
   templateUrl: './contract-item.component.html',
-  styleUrls: ['./contract-item.component.scss']
+  styleUrls: ['./contract-item.component.scss'],
 })
 export class ContractItemComponent implements OnInit {
+  @Output() submit = new EventEmitter<void>();
+  submitted = false;
+  contract: any = { code: 'ORC-001/2020-NRT/DAQ-00' };
+  validation = (contract_validation as any).default;
+  DEPARTMENTS = ['DPC', 'DAQ', 'DEC', 'DRM'];
+  COORDINATIONS = [
+    'Gerenciamento de Obras',
+    'Instalações',
+    'Impermeabilização',
+    'Projetos Arquitetônicos',
+    'Design de Interiores',
+    'Sistemas Elétricos',
+    'Sistemas Hidrosanitários',
+    'Sistemas Estruturais',
+    'Recursos Hidricos',
+    'Meio Ambiente',
+  ];
 
-  constructor() { }
+  constructor(private contractService: ContractService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  registerContract(): void {
+    this.submitted = true;
+    this.contractService.saveContract(this.contract);
+    this.submit.emit();
   }
-
 }
