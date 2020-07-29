77cd74bd5aa2ed275c9f42cca25a267f9bd67e8e
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-07-29 11:01:21 -0300

    Finalize contract edition feature

--- src/app/pages/contracts/contract-item/contract-item.component.ts
+++ src/app/pages/contracts/contract-item/contract-item.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { ContractService } from '../../../shared/services/contract.service';
-import { take } from 'rxjs/operators';
-import { DepartmentService } from '../../../shared/services/department.service';
-import * as contract_validation from '../../../shared/invoice-validation.json';
+import * as contract_validation from '../../../shared/contract-validation.json';
 
 @Component({
   selector: 'ngx-contract-item',
@@ -15,28 +13,27 @@ export class ContractItemComponent implements OnInit {
   submitted = false;
   contractNumber: number;
   validation = (contract_validation as any).default;
-  STATOOS = ['Em análise', 'Fechado', 'Negado'];
-  INTERESTS = [...Array(24).keys()].map((index) => index + 1);
+  STATOOS = ['Em andamento', 'Concluído', 'Arquivado'];
+  INTERESTS = [...Array(24).keys()].map((index) => (index + 1).toString());
   paymentIcon = {
     icon: 'dollar-sign',
     pack: 'fa',
   };
 
-  constructor(
-    private contractService: ContractService,
-    private departmentService: DepartmentService
-  ) {}
+  constructor(private contractService: ContractService) {}
 
   ngOnInit(): void {
     this.contract.interest = this.contract.payments.length;
     this.contract.paid = 0; // Sum all payments
+    console.log(this.contract);
   }
 
   registerContract(): void {
-    this.contract.department = this.departmentService.extractAbreviation(
-      this.contract.department
-    );
+    console.log(this.contract);
     this.submitted = true;
+    let version = +this.contract.version;
+    version += 1;
+    this.contract.version = version.toString().padStart(2, '0');
     this.contractService.editContract(this.contract);
     this.submit.emit();
   }
