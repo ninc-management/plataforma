4f1f71398f5579a9b6eb86e2f1700ccfc1a4944b
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2020-08-21 13:44:11 -0300

    Welcome message

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+  constructor(public userService: UserService) {}
 }
