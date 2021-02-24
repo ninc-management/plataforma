24b3f8828c4d1cfbc38fa8253c17299b9e45ef75
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2021-02-24 11:34:27 -0300

    Feedback fixes

--- src/app/pages/contracts/contract-item/contract-item.component.ts
+++ src/app/pages/contracts/contract-item/contract-item.component.ts
@@ -71,7 +71,9 @@ export class ContractItemComponent implements OnInit {
     if (!this.contract.team || this.contract.team?.length == 0) {
       this.contract.team = _.cloneDeep(this.contract.invoice.team);
       this.contract.team = this.contract.team.map((member) => {
-        member.user = this.userService.idToUser(member.user);
+        member.user = member.user?._id
+          ? member.user
+          : this.userService.idToUser(member.user);
         return member;
       });
       this.contract.team.unshift({
