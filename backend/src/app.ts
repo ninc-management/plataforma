7bd30cd16d2aa79b1f93ac968b10bcac06bfe8c9
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2021-05-28 09:26:34 -0300

    Increase query size limit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -49,8 +49,8 @@ class NortanAPI {
       })
     );
     this.app.use(cors());
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json({ limit: '50mb' }));
+    this.app.use(express.urlencoded({ extended: false, limit: '50mb' }));
     this.app.use('/', express.static(path.join(__dirname, 'angular')));
 
     // API endpoint routes
