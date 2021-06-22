c024243ea5deeccc8422084d938f15020790f1e9
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2021-06-22 15:05:48 -0300

    Add types for socket.io-mock

---
+++ src/types/socketio-mock.d.ts
@@ -0,0 +1,50 @@
+type Emitter = {
+  new (obj?: Partial<Emitter>): Emitter;
+  _callbacks: { [event: string]: Function[] };
+  emit: (event: string, ...args: []) => Emitter;
+  listeners: (event: string) => Function[];
+  hasListeners: (event: string) => boolean;
+  on: (event: string, callback: (payload?: unknown) => void) => Emitter;
+  addEventListener: (
+    event: string,
+    callback: (payload?: unknown) => void
+  ) => Emitter;
+  off: (event?: string, fn?: Function) => Emitter;
+  removeListener: (event: string, fn: Function) => Emitter;
+  removeAllListeners: () => Emitter;
+  removeEventListeners: (event: string) => Emitter;
+};
+export type SocketClient = Emitter & {
+  new (socketMock: SocketMock): SocketClient;
+  connected: boolean;
+  disconnected: boolean;
+  _socketMock: SocketMock;
+  _emitFn: (event: string, ...args: []) => Emitter;
+  emit: (eventKey: string, payload?: unknown, ack?: () => void) => void;
+  fireEvent: (eventKey: string, payload: unknown) => void;
+  close: () => SocketClient;
+  disconnect: () => SocketClient;
+};
+export type SocketMock = Emitter & {
+  new (): SocketMock;
+  generalCallbacks: {};
+  joinedRooms: string[];
+  rooms: string[];
+  socketClient: SocketClient;
+  broadcast: {
+    to: (roomKey: string) => {
+      emit: (eventKey: string, payload?: unknown) => void;
+    };
+  };
+  _emitFn: () => void;
+  emitEvent: (eventKey: string, payload?: unknown, ack?: () => void) => void;
+  onEmit: (
+    eventKey: string,
+    callback: (payload?: string, roomKey?: string) => void
+  ) => void;
+  emit: (eventKey: string, payload?: unknown) => void;
+  join: (roomKey: string) => void;
+  leave: (roomKey: string) => void;
+  monitor: (value: string) => string;
+  disconnect: (callback?: () => void) => SocketMock;
+};
