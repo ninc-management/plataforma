45f0aa7dbb69cf637809cbbc902d21523b4fc2a7
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2021-08-20 12:28:31 -0300

    Add nortan expenses table

--- src/app/pages/dashboard/nortan-expenses/nortan-expenses.component.ts
+++ src/app/pages/dashboard/nortan-expenses/nortan-expenses.component.ts
@@ -1,12 +1,175 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Expense } from '@models/expense';
+import { NbDialogService } from '@nebular/theme';
+import {
+  NortanService,
+  NORTAN_EXPENSE_TYPES,
+} from 'app/shared/services/nortan.service';
+import { UserService } from 'app/shared/services/user.service';
+import { UtilsService } from 'app/shared/services/utils.service';
+import { cloneDeep } from 'lodash';
+import { LocalDataSource } from 'ng2-smart-table';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+import {
+  DashboardDialogComponent,
+  DASHBOARD_COMPONENT_TYPES,
+} from '../dashboard-dialog/dashboard-dialog.component';
 
 @Component({
   selector: 'ngx-nortan-expenses',
   templateUrl: './nortan-expenses.component.html',
   styleUrls: ['./nortan-expenses.component.scss'],
 })
-export class NortanExpensesComponent implements OnInit {
-  constructor() {}
+export class NortanExpensesComponent implements OnInit, OnDestroy {
+  @Input() isDialogBlocked = new BehaviorSubject<boolean>(false);
+  destroy$ = new Subject<void>();
+  expenses: Expense[] = [];
+  source: LocalDataSource = new LocalDataSource();
+  settings = {
+    mode: 'external',
+    noDataMessage: 'Não encontramos nenhum gasto para o filtro selecionado.',
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: false,
+    },
+    actions: {
+      columnTitle: 'Ações',
+      add: true,
+      edit: true,
+      delete: false,
+    },
+    columns: {
+      code: {
+        title: '#',
+        type: 'string',
+        sortDirection: 'desc',
+        width: '5%',
+        compareFunction: this.itemSort,
+      },
+      source: {
+        title: 'Fonte',
+        type: 'string',
+      },
+      description: {
+        title: 'Descrição',
+        type: 'string',
+      },
+      value: {
+        title: 'Valor',
+        type: 'string',
+        compareFunction: this.valueSort,
+      },
+      type: {
+        title: 'Categoria',
+        type: 'string',
+        filter: {
+          type: 'list',
+          config: {
+            selectText: 'Todos',
+            list: Object.values(NORTAN_EXPENSE_TYPES).map((type) => ({
+              value: type,
+              title: type,
+            })),
+          },
+        },
+      },
+      created: {
+        title: 'Data',
+        type: 'string',
+      },
+    },
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    private nortanService: NortanService,
+    private userService: UserService,
+    private dialogService: NbDialogService,
+    public utils: UtilsService
+  ) {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.nortanService
+      .getExpenses()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((expenses) => {
+        this.expenses = expenses;
+        this.source.load(
+          expenses.map((expense: any) => {
+            const tmp = cloneDeep(expense);
+            tmp.source = this.userService.idToShortName(tmp.source);
+            tmp.created = this.utils.formatDate(tmp.created);
+            return tmp;
+          })
+        );
+      });
+  }
+
+  openDialog(index?: number): void {
+    this.isDialogBlocked.next(true);
+    this.dialogService
+      .open(DashboardDialogComponent, {
+        context: {
+          title:
+            index !== undefined
+              ? 'EDITAR GASTO NORTAN'
+              : 'ADICIONAR GASTO NORTAN',
+          iExpense: index !== undefined ? this.expenses[index] : undefined,
+          componentType: DASHBOARD_COMPONENT_TYPES.EXPENSE,
+        },
+        dialogClass: 'my-dialog',
+        closeOnBackdropClick: false,
+        closeOnEsc: false,
+        autoFocus: false,
+      })
+      .onClose.pipe(take(1))
+      .subscribe(() => {
+        this.isDialogBlocked.next(false);
+      });
+  }
+
+  expenseIndex(code: 'string'): number {
+    return this.expenses.findIndex((expense) => expense.code == code);
+  }
+
+  valueSort(direction: number, a: string, b: string): number {
+    const first = +a.replace(/[,.]/g, '');
+    const second = +b.replace(/[,.]/g, '');
+
+    if (first < second) {
+      return -1 * direction;
+    }
+    if (first > second) {
+      return direction;
+    }
+    return 0;
+  }
+
+  itemSort(direction: number, a: string, b: string): number {
+    const first = +a.replace(/[#]/g, '');
+    const second = +b.replace(/[#]/g, '');
+
+    if (first < second) {
+      return -1 * direction;
+    }
+    if (first > second) {
+      return direction;
+    }
+    return 0;
+  }
 }
