cfc13e9bc4b4d49c6d2f769fc19df44f32b5c7c7
Elygledson Js <Elygledsonjs@gmail.com>
2021-08-27 13:25:55 -0300

    adding rotation to the plus icon of the button.

--- src/app/pages/invoices/invoice-dialog/invoice-dialog.component.spec.ts
+++ src/app/pages/invoices/invoice-dialog/invoice-dialog.component.spec.ts
@@ -2,14 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InvoiceDialogComponent } from './invoice-dialog.component';
 import { CommonTestingModule } from 'app/../common-testing.module';
+import { NbIconLibraries } from '@nebular/theme';
 
 describe('InvoiceDialogComponent', () => {
   let component: InvoiceDialogComponent;
   let fixture: ComponentFixture<InvoiceDialogComponent>;
+  let iconsLibrary: NbIconLibraries;
 
   CommonTestingModule.setUpTestBed(InvoiceDialogComponent);
 
   beforeEach(() => {
+    iconsLibrary = TestBed.inject(NbIconLibraries);
+    iconsLibrary.registerFontPack('far', {
+      packClass: 'far',
+      iconClassPrefix: 'fa',
+    });
     fixture = TestBed.createComponent(InvoiceDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
