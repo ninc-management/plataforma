4a79a27b2aecd8ac79e904905ecfd9811a8bf85d
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2021-08-28 19:18:50 -0300

    Remove similar colors from light graph palette

--- src/app/@theme/styles/theme.default.ts
+++ src/app/@theme/styles/theme.default.ts
@@ -6,8 +6,6 @@ if (baseTheme.variables) {
   const baseThemeVariables = baseTheme.variables;
   const pallete = [
     '#37A2DA',
-    '#32C5E9',
-    '#67E0E3',
     '#9FE6B8',
     '#FFDB5C',
     '#ff9f7f',
@@ -16,8 +14,6 @@ if (baseTheme.variables) {
     '#E690D1',
     '#e7bcf3',
     '#9d96f5',
-    '#8378EA',
-    '#96BFFF',
   ];
   theme = {
     name: 'default',
