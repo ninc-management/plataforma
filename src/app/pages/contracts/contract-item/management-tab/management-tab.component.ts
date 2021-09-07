44fdc2c40fdf5a97112852564afd56bf5219d21b
leo-holanda <leo.uldholanda@gmail.com>
2021-09-07 16:58:46 -0300

    Add initial fields

--- src/app/pages/contracts/contract-item/management-tab/management-tab.component.ts
+++ src/app/pages/contracts/contract-item/management-tab/management-tab.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contract } from '@models/contract';
+import { Invoice } from '@models/invoice';
+import * as contract_validation from 'app/shared/contract-validation.json';
+import { ContractorService } from 'app/shared/services/contractor.service';
+import { InvoiceService } from 'app/shared/services/invoice.service';
+import { UserService } from 'app/shared/services/user.service';
 
 @Component({
   selector: 'ngx-management-tab',
@@ -6,7 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./management-tab.component.scss'],
 })
 export class ManagementTabComponent implements OnInit {
-  constructor() {}
+  @Input() contract!: Contract;
+  validation = (contract_validation as any).default;
+  invoice!: Invoice;
+  responsible!: string;
 
-  ngOnInit(): void {}
+  managementStatus = '';
+  avaliableStatus = [
+    'Produção',
+    'Análise Externa',
+    'Espera',
+    'Prioridade',
+    'Finalização',
+    'Concluído',
+  ];
+
+  constructor(
+    private invoiceService: InvoiceService,
+    private contractorService: ContractorService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.contract.invoice) {
+      this.invoice = this.invoiceService.idToInvoice(this.contract.invoice);
+    }
+    this.responsible = this.userService.idToName(this.invoice.author);
+  }
+
+  tooltipText(): string {
+    if (this.contract.invoice) {
+      const invoice = this.invoiceService.idToInvoice(this.contract.invoice);
+      if (invoice.contractor)
+        return (
+          `CPF/CNPJ: ` +
+          this.contractorService.idToContractor(invoice.contractor).document +
+          `\nEmail: ` +
+          this.contractorService.idToContractor(invoice.contractor).email +
+          `\nEndereço: ` +
+          this.contractorService.idToContractor(invoice.contractor).address
+        );
+    }
+    return '';
+  }
 }
