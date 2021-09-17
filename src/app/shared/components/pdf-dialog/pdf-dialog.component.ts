a6aed43e978ebdbf47f1a08734bd95a9b7fab2aa
Wilamis Micael de Araújo Aviz <wmaa@ic.ufal.br>
2021-09-17 10:21:56 -0300

    Put the pdf-dialog to close by pressing ESC

--- src/app/shared/components/pdf-dialog/pdf-dialog.component.ts
+++ src/app/shared/components/pdf-dialog/pdf-dialog.component.ts
@@ -2,27 +2,31 @@ import { Component, OnInit, Input, Inject, Optional } from '@angular/core';
 import { NbDialogRef, NB_DOCUMENT } from '@nebular/theme';
 import { Subject } from 'rxjs';
 import { take } from 'rxjs/operators';
+import { BaseDialogComponent } from '../base-dialog/base-dialog.component';
 
 @Component({
   selector: 'ngx-pdf-dialog',
   templateUrl: './pdf-dialog.component.html',
   styleUrls: ['./pdf-dialog.component.scss'],
 })
-export class PdfDialogComponent implements OnInit {
+export class PdfDialogComponent extends BaseDialogComponent implements OnInit {
   @Input() dataUrl$ = new Subject<string>();
   data = '';
 
   constructor(
-    @Inject(NB_DOCUMENT) protected document: Document,
-    @Optional() protected ref: NbDialogRef<PdfDialogComponent>
-  ) {}
+    @Inject(NB_DOCUMENT) protected derivedDocument: Document,
+    @Optional() protected derivedRef: NbDialogRef<PdfDialogComponent>
+  ) {
+    super(derivedDocument, derivedRef);
+  }
 
   ngOnInit(): void {
+    super.ngOnInit();
     this.dataUrl$.pipe(take(1)).subscribe((data) => (this.data = data));
   }
 
   dismiss(): void {
-    this.ref.close();
+    this.derivedRef.close();
   }
 
   windowWidth(): number {
