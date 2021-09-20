6b49220b6574a1f72d1dbd8edcdc4e9011b3e1a3
leo-holanda <leo.uldholanda@gmail.com>
2021-09-20 10:43:15 -0300

    Add checklist item component

---
+++ src/app/pages/contracts/contract-item/management-tab/checklist/checklist-item/checklist-item.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ContractChecklistItem } from '@models/contract';
+import { User } from '@models/user';
+import * as contract_validation from 'app/shared/contract-validation.json';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'ngx-checklist-item',
+  templateUrl: './checklist-item.component.html',
+  styleUrls: ['./checklist-item.component.scss'],
+})
+export class ChecklistItemComponent implements OnInit {
+  @Input() checklistItem: ContractChecklistItem = new ContractChecklistItem();
+  validation = (contract_validation as any).default;
+  today = new Date();
+  yesterday = new Date();
+  responsibleSearch = '';
+  avaliableResponsibles: Observable<User[]> = of([]);
+
+  avaliableActionStatus = [
+    'Briefing',
+    'Anteprojeto',
+    'Estudo preliminar',
+    'Projeto básico',
+    'Projeto executivo',
+    'Campo',
+    'Prioridade',
+    'Análise externa',
+    'Espera',
+    'Finalização',
+    'Concluído',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.yesterday.setDate(this.today.getDate() - 1);
+  }
+}
