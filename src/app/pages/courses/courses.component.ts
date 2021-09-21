36fb490a3ca7232360549f232ef9f87c060d450c
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2021-09-21 10:34:18 -0300

    Create course components and add to sidebar menu
    Co-authored-by: Mauricésar <mauricesar@users.noreply.github.com>
    Co-authored-by: Leo Holanda <leo.uldholanda@gmail.com>
    Co-authored-by: Elygledson Js <Elygledson@users.noreply.github.com>

---
+++ src/app/pages/courses/courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
