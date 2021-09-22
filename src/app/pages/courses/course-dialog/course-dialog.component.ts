7cd12fc856608b105f7ee4ef6f1f535c55333a6f
Augusto Icaro Farias da Cunha <contact@augustoicaro.com>
2021-09-22 12:06:11 -0300

    Make course dialog extends dialog base

--- src/app/pages/courses/course-dialog/course-dialog.component.ts
+++ src/app/pages/courses/course-dialog/course-dialog.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { NbDialogRef, NB_DOCUMENT } from '@nebular/theme';
+import { BaseDialogComponent } from 'app/shared/components/base-dialog/base-dialog.component';
 
 @Component({
-  selector: 'course-dialog',
+  selector: 'ngx-course-dialog',
   templateUrl: './course-dialog.component.html',
-  styleUrls: ['./course-dialog.component.scss']
+  styleUrls: ['./course-dialog.component.scss'],
 })
-export class CourseDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CourseDialogComponent extends BaseDialogComponent implements OnInit {
+  constructor(
+    @Inject(NB_DOCUMENT) protected derivedDocument: Document,
+    @Optional() protected derivedRef: NbDialogRef<CourseDialogComponent>
+  ) {
+    super(derivedDocument, derivedRef);
   }
 
+  ngOnInit(): void {
+    super.ngOnInit();
+  }
+
+  dismiss(): void {
+    super.dismiss();
+  }
 }
