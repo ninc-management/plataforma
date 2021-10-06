2179c6c4d40b5a9b9963a2d753afdd3837492893
leo-holanda <leo.uldholanda@gmail.com>
2021-10-06 13:47:55 -0300

    Improve numberToMoney pipe test

--- src/app/shared/pipes/string-util.pipe.spec.ts
+++ src/app/shared/pipes/string-util.pipe.spec.ts
@@ -3,19 +3,23 @@ import { BrMaskDirective } from '../directives/br-mask.directive';
 import { NumberToMoneyPipe } from './string-util.pipe';
 
 describe('StringUtilPipes', inject([BrMaskDirective], (brMask: BrMaskDirective) => {
+  let numberToMoneyPipe: NumberToMoneyPipe;
+
+  beforeEach(() => {
+    numberToMoneyPipe = new NumberToMoneyPipe(brMask);
+  });
+
   it('create an numberToMoneyPipe instance', () => {
-    const pipe = new NumberToMoneyPipe(brMask);
-    expect(pipe).toBeTruthy();
+    expect(numberToMoneyPipe).toBeTruthy();
   });
 
   it('numberToMoneyPipe shoud work', () => {
-    const pipe = new NumberToMoneyPipe(brMask);
-    expect(pipe.transform(100.0)).toBe('100,00');
-    expect(pipe.transform(0.0)).toBe('0,00');
-    expect(pipe.transform(1000.0)).toBe('1.000,00');
-    expect(pipe.transform(1000.0, 4)).toBe('1.000,0000');
-    expect(pipe.transform(-1000.0)).toBe('-1.000,00');
-    expect(pipe.transform(350000.0)).toBe('350.000,00');
-    expect(pipe.transform(1000000.0)).toBe('1.000.000,00');
+    expect(numberToMoneyPipe.transform(100.0)).toBe('100,00');
+    expect(numberToMoneyPipe.transform(0.0)).toBe('0,00');
+    expect(numberToMoneyPipe.transform(1000.0)).toBe('1.000,00');
+    expect(numberToMoneyPipe.transform(1000.0, 4)).toBe('1.000,0000');
+    expect(numberToMoneyPipe.transform(-1000.0)).toBe('-1.000,00');
+    expect(numberToMoneyPipe.transform(350000.0)).toBe('350.000,00');
+    expect(numberToMoneyPipe.transform(1000000.0)).toBe('1.000.000,00');
   });
 }));
