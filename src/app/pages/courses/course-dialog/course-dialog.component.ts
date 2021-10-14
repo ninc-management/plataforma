0815d91362b43d9fd1dab0e5279c12a4434e0d5f
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2021-10-14 16:51:16 -0300

    NWS-953: Add course dialog component

--- src/app/pages/courses/course-dialog/course-dialog.component.ts
+++ src/app/pages/courses/course-dialog/course-dialog.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { Component, OnInit, Inject, Optional, Input } from '@angular/core';
 import { NbDialogRef, NB_DOCUMENT } from '@nebular/theme';
 import { BaseDialogComponent } from 'app/shared/components/base-dialog/base-dialog.component';
+import { UtilsService } from 'app/shared/services/utils.service';
+import { Course, CourseParticipant } from '@models/course';
 
+export enum DIALOG_TYPES {
+  COURSE,
+  PARTICIPANT,
+}
 @Component({
   selector: 'ngx-course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.scss'],
 })
 export class CourseDialogComponent extends BaseDialogComponent implements OnInit {
+  @Input() title = '';
+  @Input() course = new Course();
+  @Input() participant = new CourseParticipant();
+  @Input() componentType = DIALOG_TYPES.COURSE;
+  dTypes = DIALOG_TYPES;
+
   constructor(
     @Inject(NB_DOCUMENT) protected derivedDocument: Document,
-    @Optional() protected derivedRef: NbDialogRef<CourseDialogComponent>
+    @Optional() protected derivedRef: NbDialogRef<CourseDialogComponent>,
+    public utils: UtilsService
   ) {
     super(derivedDocument, derivedRef);
   }
