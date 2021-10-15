111dc2ce86a0672b462f4801af83b89bd25d7b25
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2021-10-15 10:07:06 -0300

    Add course item component and form validation

--- src/app/pages/courses/course-item/course-item.component.ts
+++ src/app/pages/courses/course-item/course-item.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Course, CourseParticipant } from '@models/course';
+import { User } from '@models/user';
+import { NbDialogService } from '@nebular/theme';
+
+import * as course_validation from 'app/shared/course-validation.json';
+import { CourseService } from 'app/shared/services/course.service';
+import { UserService } from 'app/shared/services/user.service';
+import { UtilsService } from 'app/shared/services/utils.service';
+import { cloneDeep } from 'lodash';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { CourseDialogComponent, DIALOG_TYPES } from '../course-dialog/course-dialog.component';
 
 @Component({
   selector: 'ngx-course-item',
@@ -6,7 +18,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-item.component.scss'],
 })
 export class CourseItemComponent implements OnInit {
-  constructor() {}
+  @Input() iCourse = new Course();
+  @Input() isDialogBlocked = new BehaviorSubject<boolean>(false);
+  course: Course = new Course();
+  validation = (course_validation as any).default;
+  editing = false;
+  speakerSearch = '';
+  participantsSearch = '';
+  avaliableSpeakers: Observable<User[]> = of([]);
+  avaliableParticipants: Observable<User[]> = of([]);
+  currentParticipant = new CourseParticipant();
 
-  ngOnInit(): void {}
+  constructor(
+    private courseService: CourseService,
+    public userService: UserService,
+    public utils: UtilsService,
+    private dialogService: NbDialogService
+  ) {}
+
+  ngOnInit(): void {
+    this.avaliableSpeakers = this.userService.getUsers();
+    this.avaliableParticipants = this.userService.getUsers();
+  }
+
+  createCourse() {
+    console.log(this.course);
+    // this.courseService.saveCourse(this.course);
+  }
+
+  addSpeaker() {
+    console.log('added');
+  }
+
+  addParticipant(): void {
+    this.isDialogBlocked.next(true);
+    this.dialogService
+      .open(CourseDialogComponent, {
+        context: {
+          title: 'REGISTRAR' + this.editing ? 'PARTICIPANTE' : 'MINISTRANTE',
+          componentType: DIALOG_TYPES.PARTICIPANT,
+        },
+        dialogClass: 'my-dialog',
+        closeOnBackdropClick: false,
+        closeOnEsc: false,
+        autoFocus: false,
+      })
+      .onClose.pipe(take(1))
+      .subscribe(() => {
+        this.isDialogBlocked.next(false);
+      });
+  }
 }
