fcd422ffb4658e7fed7d80efb60bb9b578be8136
leo-holanda <leo.uldholanda@gmail.com>
2021-10-19 15:04:00 -0300

    Add participant registration in courses table

--- src/app/pages/courses/participant-item/participant-item.component.ts
+++ src/app/pages/courses/participant-item/participant-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Course, CourseParticipant } from '@models/course';
 import * as participant_validation from 'app/shared/participant-validation.json';
+import { CourseService } from 'app/shared/services/course.service';
 import { cloneDeep } from 'lodash';
 
 @Component({
@@ -14,12 +15,14 @@ export class ParticipantItemComponent implements OnInit {
   participant = new CourseParticipant();
   validation = (participant_validation as any).default;
 
-  constructor() {}
+  constructor(private courseService: CourseService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.participant = cloneDeep(this.iParticipant);
+  }
 
-  createParticipant() {
-    this.iParticipant = cloneDeep(this.participant);
+  createParticipant(): void {
     this.iCourse.participants.push(cloneDeep(this.participant));
+    this.courseService.editCourse(this.iCourse);
   }
 }
