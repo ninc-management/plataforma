176f12323a808f339e2b2d41f4a581420c1a0498
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2021-10-19 10:29:14 -0300

    Create function to add participant to course

--- src/app/pages/courses/participant-item/participant-item.component.ts
+++ src/app/pages/courses/participant-item/participant-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CourseParticipant } from '@models/course';
+import { Course, CourseParticipant } from '@models/course';
 import * as participant_validation from 'app/shared/participant-validation.json';
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'ngx-participant-item',
@@ -9,6 +10,7 @@ import * as participant_validation from 'app/shared/participant-validation.json'
 })
 export class ParticipantItemComponent implements OnInit {
   @Input() iParticipant = new CourseParticipant();
+  @Input() iCourse = new Course();
   participant = new CourseParticipant();
   validation = (participant_validation as any).default;
 
@@ -17,6 +19,7 @@ export class ParticipantItemComponent implements OnInit {
   ngOnInit(): void {}
 
   createParticipant() {
-    console.log('created');
+    this.iParticipant = cloneDeep(this.participant);
+    this.iCourse.participants.push(cloneDeep(this.participant));
   }
 }
