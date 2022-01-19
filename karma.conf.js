42a4440948c5c4103d4124354be0232fc5e792a3
Augusto Ícaro Farias da Cunha <contact@augustoicaro.com>
2022-01-19 06:49:16 +0000

    Increse response timeout to best work with Gitpod

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,9 @@ module.exports = function (config) {
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        timeoutInterval: 15000,
+      },
     },
     coverageReporter: {
       dir: require('path').join(__dirname, 'coverage'),
