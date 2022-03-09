b0bf6b8daab79acd8341e6484abc22f79bf2d11f
Leo Holanda <lulh@ic.ufal.br>
2022-03-09 22:58:14 +0000

    Add property pipe

---
+++ src/app/shared/pipes/property-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PropertyPipe } from './property-pipe.pipe';
+
+describe('PropertyPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PropertyPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/pipes/property-pipe.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+type GetElementType<T extends any[]> = T extends (infer U)[] ? U : never;
+
+@Pipe({
+  name: 'propertyPipe',
+})
+export class PropertyPipe implements PipeTransform {
+  transform<F extends (...args: any[]) => ReturnType<F>>(
+    fnArguments: Parameters<F>,
+    fnReference: F,
+    property?: keyof NonNullable<Exclude<GetElementType<Parameters<F>>, string>>
+  ): string {
+    return property ? (fnReference(...fnArguments) as any)[property] : fnReference(...fnArguments);
+  }
+}
