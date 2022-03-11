e236ad900dbe48635697590feaad4fbe921054d8
Leo Holanda <lulh@ic.ufal.br>
2022-03-11 19:23:43 +0000

    Add user receivable value in dashboard metrics

--- src/app/shared/services/metrics.service.ts
+++ src/app/shared/services/metrics.service.ts
@@ -9,6 +9,8 @@ import { UtilsService } from './utils.service';
 import { cloneDeep, mergeWith, add } from 'lodash';
 import { format } from 'date-fns';
 import { TeamService } from './team.service';
+import { Contract } from '@models/contract';
+import { ContractorService } from './contractor.service';
 
 export type TimeSeriesItem = [string, number];
 
@@ -88,7 +90,8 @@ export class MetricsService implements OnDestroy {
     private userService: UserService,
     private stringUtil: StringUtilService,
     private utils: UtilsService,
-    private teamService: TeamService
+    private teamService: TeamService,
+    private contractorService: ContractorService
   ) {
     this.teamService
       .getTeams()
@@ -741,5 +744,88 @@ export class MetricsService implements OnDestroy {
         return this.utils.groupByDateTimeSerie(timeSeriesItems);
       })
     );
+  }
+
+  userReceivableValue(uId: string): Observable<MetricInfo> {
+    return combineLatest([this.contractService.getContracts(), this.invoiceService.getInvoices()]).pipe(
+      filter(([contracts, invoices]) => contracts.length > 0 && invoices.length > 0),
+      map(([contracts, invoices]) => {
+        return contracts.reduce(
+          (userReceivable: MetricInfo, contract) => {
+            if (contract.invoice && contract.status != CONTRACT_STATOOS.ARQUIVADO) {
+              const invoice = this.invoiceService.idToInvoice(contract.invoice);
+              const member = invoice.team.find((member) => this.userService.isEqual(member.user, uId));
+
+              if (member) {
+                contract = this.fillContract(contract);
+                const notPaid = this.contractService.notPaidValue(member.distribution, member.user, contract);
+                const cashback = this.stringUtil.numberToMoney(
+                  this.contractService.expensesContributions(contract, member.user).user.cashback
+                );
+
+                userReceivable.value += this.stringUtil.moneyToNumber(this.stringUtil.sumMoney(notPaid, cashback));
+              }
+            }
+
+            return userReceivable;
+          },
+          { count: 0, value: 0 }
+        );
+      })
+    );
+  }
+
+  private fillContract(contract: Contract): Contract {
+    if (contract.invoice) {
+      const invoice = this.invoiceService.idToInvoice(contract.invoice);
+      contract.invoice = invoice;
+
+      if (invoice.author) {
+        const managerPicture = this.userService.idToUser(invoice.author).profilePicture;
+        if (managerPicture) contract.managerPicture = managerPicture;
+        contract.fullName = this.userService.idToShortName(invoice.author);
+      }
+
+      if (invoice.contractor) {
+        contract.contractor = this.contractorService.idToName(invoice.contractor);
+      }
+
+      contract.name = invoice.name;
+      contract.interests = contract.receipts.length.toString() + '/' + contract.total;
+      this.userService.currentUser$.pipe(take(1)).subscribe((user) => {
+        contract.role = this.invoiceService.role(invoice, user);
+      });
+
+      const nf = this.utils.nfPercentage(contract);
+      const nortan = this.utils.nortanPercentage(contract);
+      contract.value = contract.invoice.value;
+      contract.code = contract.invoice.code;
+      contract.balance = this.contractService.balance(contract);
+      contract.liquid = this.contractService.toNetValue(
+        this.contractService.subtractComissions(
+          this.stringUtil.removePercentage(contract.value, contract.ISS),
+          contract
+        ),
+        this.utils.nfPercentage(contract),
+        this.utils.nortanPercentage(contract)
+      );
+
+      const paid = this.contractService.toNetValue(
+        this.stringUtil.numberToMoney(
+          contract.receipts.reduce((accumulator: number, recipt: any) => {
+            if (recipt.paid) accumulator = accumulator + this.stringUtil.moneyToNumber(recipt.value);
+            return accumulator;
+          }, 0)
+        ),
+        nf,
+        nortan
+      );
+
+      contract.notPaid = this.stringUtil.numberToMoney(
+        this.stringUtil.moneyToNumber(this.contractService.toNetValue(contract.value, nf, nortan)) -
+          this.stringUtil.moneyToNumber(paid)
+      );
+    }
+    return contract;
   }
 }
