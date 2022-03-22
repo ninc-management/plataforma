e7eed98fdff0d6542762aa816151487315407970
Leo Holanda <lulh@ic.ufal.br>
2022-03-22 18:17:05 +0000

    Add expense types config in config component

--- backend/src/models/platformConfig.ts
+++ backend/src/models/platformConfig.ts
@@ -2,9 +2,9 @@ import { prop, getModelForClass } from '@typegoose/typegoose';
 import { Base } from './base';
 import { ExpenseType } from './team';
 
-export class platformConfig extends Base<string> {
+export class PlatformConfig extends Base<string> {
   @prop({ required: true, type: () => [ExpenseType] })
   expenseTypes: ExpenseType[] = [];
 }
 
-export default getModelForClass(platformConfig);
+export default getModelForClass(PlatformConfig);
