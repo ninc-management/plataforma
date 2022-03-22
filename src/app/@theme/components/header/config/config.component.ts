345acb5eb921dbbee09429954ef81b373b87d976
Leo Holanda <lulh@ic.ufal.br>
2022-03-22 18:17:05 +0000

    Add expense types config in config component

--- src/app/@theme/components/header/config/config.component.ts
+++ src/app/@theme/components/header/config/config.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PlatformConfig } from '@models/platformConfig';
+import { ExpenseType } from '@models/team';
+import { UtilsService } from 'app/shared/services/utils.service';
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'ngx-config',
@@ -6,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config.component.scss'],
 })
 export class ConfigComponent implements OnInit {
-  constructor() {}
+  config: PlatformConfig = new PlatformConfig();
+  newExpense: ExpenseType = new ExpenseType();
+
+  constructor(public utils: UtilsService) {}
 
   ngOnInit(): void {}
+
+  addExpenseType(): void {
+    this.config.expenseTypes.push(cloneDeep(this.newExpense));
+    this.newExpense = new ExpenseType();
+  }
 }
