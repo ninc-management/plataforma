4428bffe7ce57ac1c709fa5368ff84178c12f129
Leo Holanda <lulh@ic.ufal.br>
2022-03-28 23:08:10 +0000

    Fix team expense item test

--- src/app/shared/components/teams/team-expenses/team-expense-item/team-expense-item.component.spec.ts
+++ src/app/shared/components/teams/team-expenses/team-expense-item/team-expense-item.component.spec.ts
@@ -2,17 +2,59 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TeamExpenseItemComponent } from './team-expense-item.component';
 import { CommonTestingModule } from 'app/../common-testing.module';
+import { ConfigService } from 'app/shared/services/config.service';
+import { of } from 'rxjs';
+import { PlatformConfig } from '@models/platformConfig';
+import { ExpenseType } from '@models/team';
+import { cloneDeep } from 'lodash';
+import { HttpTestingController } from '@angular/common/http/testing';
 
-describe('NortanExpenseItemComponent', () => {
+describe('TeamExpenseItemComponent', () => {
   let component: TeamExpenseItemComponent;
   let fixture: ComponentFixture<TeamExpenseItemComponent>;
+  let configService: ConfigService;
+  let httpMock: HttpTestingController;
+  const configServiceSpy = jasmine.createSpyObj<ConfigService>('configService', ['getConfig', 'expenseSubTypes']);
 
   CommonTestingModule.setUpTestBed(TeamExpenseItemComponent);
 
   beforeEach(() => {
+    let mockedConfigs: PlatformConfig[] = [];
+    let mockedConfig = new PlatformConfig();
+    let mockedExpenseType = new ExpenseType();
+
+    mockedConfig._id = '0';
+    mockedExpenseType.name = 'mockedExpenseType1';
+    mockedExpenseType.subTypes.push('mockedExpenseSubType1');
+    mockedExpenseType.subTypes.push('mockedExpenseSubType2');
+    mockedConfig.expenseTypes.push(cloneDeep(mockedExpenseType));
+    mockedConfigs.push(cloneDeep(mockedConfig));
+
+    TestBed.overrideProvider(ConfigService, { useValue: configServiceSpy });
+    configServiceSpy.getConfig.and.returnValue(of(mockedConfigs));
+    configServiceSpy.expenseSubTypes.and.returnValue(mockedConfigs[0].expenseTypes.map((type) => type.name));
+    configService = TestBed.inject(ConfigService);
+    httpMock = TestBed.inject(HttpTestingController);
+
     fixture = TestBed.createComponent(TeamExpenseItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    const reqTeam = httpMock.expectOne('/api/team/all');
+    expect(reqTeam.request.method).toBe('POST');
+    setTimeout(() => {
+      reqTeam.flush([]);
+    }, 50);
+
+    const reqUser = httpMock.expectOne('/api/user/all');
+    expect(reqUser.request.method).toBe('POST');
+    setTimeout(() => {
+      reqUser.flush([]);
+    }, 50);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
