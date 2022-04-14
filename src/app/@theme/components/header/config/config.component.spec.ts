782c8581601b0432ad6d005c74798bb7222de2d5
Eduardo Henrique <ehfs@ic.ufal.br>
2022-04-13 23:25:50 -0300

    Create tab for invoices in platform configuration

--- src/app/@theme/components/header/config/config.component.spec.ts
+++ src/app/@theme/components/header/config/config.component.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlatformConfig } from '@models/platformConfig';
 import { CommonTestingModule } from 'common-testing.module';
+import { NbIconLibraries } from '@nebular/theme';
 
 import { ConfigComponent } from './config.component';
 
 describe('ConfigComponent', () => {
   let component: ConfigComponent;
   let fixture: ComponentFixture<ConfigComponent>;
+  let iconsLibrary: NbIconLibraries;
 
   CommonTestingModule.setUpTestBed(ConfigComponent);
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfigComponent);
+    iconsLibrary = TestBed.inject(NbIconLibraries);
+    iconsLibrary.registerFontPack('fa', {
+      packClass: 'fa',
+      iconClassPrefix: 'fa',
+    });
+    iconsLibrary.registerSvgPack('fac', {
+      minus: '<svg></svg>',
+    });
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
