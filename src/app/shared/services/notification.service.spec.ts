d584054b630ebfb226df71bacdbef068a4ccc04d
Augusto Ícaro Farias da Cunha <contact@augustoicaro.com>
2022-04-14 23:39:33 +0000

    Improve test function use

--- src/app/shared/services/notification.service.spec.ts
+++ src/app/shared/services/notification.service.spec.ts
@@ -9,7 +9,7 @@ import { cloneDeep } from 'lodash';
 import { Subject, take } from 'rxjs';
 import { UserService } from './user.service';
 import { AuthService } from 'app/auth/auth.service';
-import { HttpTestingController } from '@angular/common/http/testing';
+import { HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { SocketMock } from 'types/socketio-mock';
 import MockedServerSocket from 'socket.io-mock';
 import { Socket } from 'ngx-socket-io';
@@ -63,27 +63,27 @@ describe('NotificationService', () => {
     tmpTeam.abrev = 'T';
     tmpTeam.config.path = `test`;
     const tmpTeamMember = new TeamMember();
-    tmpTeamMember.user = mockedUsers[0];
+    tmpTeamMember.user = mockedUsers[0]._id;
     tmpTeamMember.sector = '0';
     tmpTeam.members.push(cloneDeep(tmpTeamMember));
-    tmpTeamMember.user = '1';
+    tmpTeamMember.user = mockedUsers[1]._id;
     tmpTeamMember.sector = '1';
     tmpTeam.members.push(cloneDeep(tmpTeamMember));
     mockedTeams.push(cloneDeep(tmpTeam));
 
     let tmpInvoice = new Invoice();
     tmpInvoice._id = '0';
-    tmpInvoice.author = mockedUsers[0];
+    tmpInvoice.author = mockedUsers[0]._id;
     tmpInvoice.nortanTeam = '6201b405329f446f16e1b404';
     tmpInvoice.sector = '0';
     tmpInvoice.code = 'ORC-84/2021-NRT/DAD-00';
     tmpInvoice.contractor = '0';
     tmpInvoice.value = '1.000,00';
     const tmpInvoiceMember = new InvoiceTeamMember();
-    tmpInvoiceMember.user = mockedUsers[0];
+    tmpInvoiceMember.user = mockedUsers[0]._id;
     tmpInvoiceMember.sector = '0';
     tmpInvoice.team.push(cloneDeep(tmpInvoiceMember));
-    tmpInvoiceMember.user = '1';
+    tmpInvoiceMember.user = mockedUsers[1]._id;
     tmpInvoiceMember.sector = '1';
     tmpInvoice.team.push(cloneDeep(tmpInvoiceMember));
     mockedInvoices.push(cloneDeep(tmpInvoice));
@@ -159,27 +159,17 @@ describe('NotificationService', () => {
       });
   });
 
-  it('notifyMany should work', (done: DoneFn) => {
+  it('notifyMany invoice team members should work', (done: DoneFn) => {
     const notificationBody = {
       title: 'Title Many - Users',
       tag: 'T-M',
       message: 'Message Many',
     };
 
-    const mockedNotifications: UserNotification[] = [];
-
-    const notification = new UserNotification();
-    notification.title = notificationBody.title;
-    notification.message = notificationBody.message;
-    notification.from = mockedUsers[0]._id;
-    notification.to = mockedUsers[0]._id;
-    mockedNotifications.push(cloneDeep(notification));
-    notification.to = mockedUsers[1]._id;
-    mockedNotifications.push(cloneDeep(notification));
-
     socket.socketClient.on('dbchange', (data: any) => socket$.next(data));
 
     let i = 1;
+    let req: TestRequest;
 
     userService
       .getUsers()
@@ -189,6 +179,10 @@ describe('NotificationService', () => {
           case 1: {
             i += 1;
             expect(users).toEqual(mockedUsers);
+            service.notifyMany(mockedInvoices[0].team, notificationBody);
+            req = httpMock.expectOne('/api/notify/many');
+            expect(req.request.method).toBe('POST');
+            req.flush(null);
             socket.emit('dbchange', {
               ns: {
                 coll: 'users',
@@ -198,7 +192,7 @@ describe('NotificationService', () => {
                 _id: '0',
               },
               updateDescription: {
-                updatedFields: { 'notifications.1': mockedNotifications[0] },
+                updatedFields: { 'notifications.1': req.request.body.notifications[0] },
                 removedFields: [] as any[],
               },
             });
@@ -215,21 +209,163 @@ describe('NotificationService', () => {
                 _id: '1',
               },
               updateDescription: {
-                updatedFields: { 'notifications.1': mockedNotifications[1] },
+                updatedFields: { 'notifications.1': req.request.body.notifications[1] },
                 removedFields: [] as any[],
               },
             });
             break;
           }
           case 3: {
-            const expectedNotifications = JSON.parse(JSON.stringify(mockedNotifications), (k, v) => {
+            const expectedNotifications = JSON.parse(JSON.stringify(req.request.body.notifications), (k, v) => {
               if (['created'].includes(k)) return parseISO(v);
               return v;
             }) as UserNotification[];
-            service.notifyMany(mockedInvoices[0].team, notificationBody);
-            const req = httpMock.expectOne('/api/notify/many');
+            expect(users[0].notifications).toEqual([expectedNotifications[0]]);
+            expect(users[1].notifications).toEqual([expectedNotifications[1]]);
+            done();
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      });
+  });
+
+  it('notifyMany team members should work', (done: DoneFn) => {
+    const notificationBody = {
+      title: 'Title Many - Users',
+      tag: 'T-M',
+      message: 'Message Many',
+    };
+
+    socket.socketClient.on('dbchange', (data: any) => socket$.next(data));
+
+    let i = 1;
+    let req: TestRequest;
+
+    userService
+      .getUsers()
+      .pipe(take(3))
+      .subscribe((users) => {
+        switch (i) {
+          case 1: {
+            i += 1;
+            expect(users).toEqual(mockedUsers);
+            service.notifyMany(mockedTeams[0].members, notificationBody);
+            req = httpMock.expectOne('/api/notify/many');
             expect(req.request.method).toBe('POST');
             req.flush(null);
+            socket.emit('dbchange', {
+              ns: {
+                coll: 'users',
+              },
+              operationType: 'update',
+              documentKey: {
+                _id: '0',
+              },
+              updateDescription: {
+                updatedFields: { 'notifications.1': req.request.body.notifications[0] },
+                removedFields: [] as any[],
+              },
+            });
+            break;
+          }
+          case 2: {
+            i += 1;
+            socket.emit('dbchange', {
+              ns: {
+                coll: 'users',
+              },
+              operationType: 'update',
+              documentKey: {
+                _id: '1',
+              },
+              updateDescription: {
+                updatedFields: { 'notifications.1': req.request.body.notifications[1] },
+                removedFields: [] as any[],
+              },
+            });
+            break;
+          }
+          case 3: {
+            const expectedNotifications = JSON.parse(JSON.stringify(req.request.body.notifications), (k, v) => {
+              if (['created'].includes(k)) return parseISO(v);
+              return v;
+            }) as UserNotification[];
+            expect(users[0].notifications).toEqual([expectedNotifications[0]]);
+            expect(users[1].notifications).toEqual([expectedNotifications[1]]);
+            done();
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      });
+  });
+
+  it('notifyMany users should work', (done: DoneFn) => {
+    const notificationBody = {
+      title: 'Title Many - Users',
+      tag: 'T-M',
+      message: 'Message Many',
+    };
+
+    socket.socketClient.on('dbchange', (data: any) => socket$.next(data));
+
+    let i = 1;
+    let req: TestRequest;
+
+    userService
+      .getUsers()
+      .pipe(take(3))
+      .subscribe((users) => {
+        switch (i) {
+          case 1: {
+            i += 1;
+            expect(users).toEqual(mockedUsers);
+            service.notifyMany(mockedUsers, notificationBody);
+            req = httpMock.expectOne('/api/notify/many');
+            expect(req.request.method).toBe('POST');
+            req.flush(null);
+            socket.emit('dbchange', {
+              ns: {
+                coll: 'users',
+              },
+              operationType: 'update',
+              documentKey: {
+                _id: '0',
+              },
+              updateDescription: {
+                updatedFields: { 'notifications.1': req.request.body.notifications[0] },
+                removedFields: [] as any[],
+              },
+            });
+            break;
+          }
+          case 2: {
+            i += 1;
+            socket.emit('dbchange', {
+              ns: {
+                coll: 'users',
+              },
+              operationType: 'update',
+              documentKey: {
+                _id: '1',
+              },
+              updateDescription: {
+                updatedFields: { 'notifications.1': req.request.body.notifications[1] },
+                removedFields: [] as any[],
+              },
+            });
+            break;
+          }
+          case 3: {
+            const expectedNotifications = JSON.parse(JSON.stringify(req.request.body.notifications), (k, v) => {
+              if (['created'].includes(k)) return parseISO(v);
+              return v;
+            }) as UserNotification[];
             expect(users[0].notifications).toEqual([expectedNotifications[0]]);
             expect(users[1].notifications).toEqual([expectedNotifications[1]]);
             done();
