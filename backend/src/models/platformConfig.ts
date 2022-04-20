d2e12b62d319bb582afdaff47541e6da20709149
Elygledson Js <ejsb2@ic.ufal.br>
2022-04-20 03:00:54 +0000

    Add profile config to playground

--- backend/src/models/platformConfig.ts
+++ backend/src/models/platformConfig.ts
@@ -2,6 +2,26 @@ import { prop, getModelForClass } from '@typegoose/typegoose';
 import { Base } from './base';
 import { ExpenseType } from './team';
 
+export class ProfileConfig {
+  @prop({ required: true, type: () => [String] })
+  positions: string[] = [];
+
+  @prop({ required: true })
+  hasLevels: boolean = true;
+
+  @prop({ required: true, type: () => [String] })
+  levels: string[] = [];
+
+  @prop({ required: true })
+  hasTeam: boolean = true;
+
+  @prop({ required: true })
+  hasSector: boolean = true;
+
+  @prop({ required: true })
+  hasExpertiseBySector: boolean = true;
+}
+
 export class InvoiceConfig {
   @prop({ required: true })
   hasType: boolean = true;
@@ -37,6 +57,9 @@ export class PlatformConfig extends Base<string> {
 
   @prop({ required: true })
   invoiceConfig: InvoiceConfig = new InvoiceConfig();
+
+  @prop({ required: true })
+  profileConfig: ProfileConfig = new ProfileConfig();
 }
 
 export default getModelForClass(PlatformConfig);
