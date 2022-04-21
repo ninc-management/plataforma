5e291934502426ea4ae064bbf7c49166ec4da4d3
Augusto Ícaro Farias da Cunha <contact@augustoicaro.com>
2022-04-21 05:30:48 +0000

    [NWS-1169][NWS-1228] Create transaction service

--- backend/src/models/shared.ts
+++ backend/src/models/shared.ts
@@ -1,5 +1,6 @@
 import { prop, getModelForClass, Ref } from '@typegoose/typegoose';
 import { Base } from './base';
+import { User } from './user';
 
 /**
  * Used by User and Team
@@ -23,4 +24,18 @@ export class UploadedFile {
 
   @prop({ required: true })
   url!: string;
+}
+
+/**
+ * Used by Transaction and Internal Transaction
+ */
+export class EditionHistoryItem {
+  @prop({ required: true, ref: () => User })
+  author!: Ref<User>;
+
+  @prop({ required: true })
+  comment!: string;
+
+  @prop({ required: true })
+  date: Date = new Date();
 }
