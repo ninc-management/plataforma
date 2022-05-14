fb1f6eed70a06130e9393df61e4133871385dbbd
Elygledson Js <ejsb2@ic.ufal.br>
2022-05-14 00:43:33 +0000

    [NWS-1172][NWS-1270] Change delay to 0s and scale to 1.3

---
+++ src/app/@theme/components/header/animation.ts
@@ -0,0 +1,47 @@
+import { animation, style, animate, keyframes } from '@angular/animations';
+
+export const tada = animation(
+  animate(
+    '1s 0s',
+    keyframes([
+      style({ transform: 'scale3d(1, 1, 1)', offset: 0 }),
+      style({
+        transform: 'scale3d(.9, .9, .9) rotate3d(0, 0, 1, -3deg)',
+        offset: 0.1,
+      }),
+      style({
+        transform: 'scale3d(.9, .9, .9) rotate3d(0, 0, 1, -3deg)',
+        offset: 0.2,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, 3deg)',
+        offset: 0.3,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, -3deg)',
+        offset: 0.4,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, 3deg)',
+        offset: 0.5,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, -3deg)',
+        offset: 0.6,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, 3deg)',
+        offset: 0.7,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, -3deg)',
+        offset: 0.8,
+      }),
+      style({
+        transform: 'scale3d(1.3, 1.3, 1.3) rotate3d(0, 0, 1, 3deg)',
+        offset: 0.9,
+      }),
+      style({ transform: 'scale3d(1, 1, 1)', offset: 1 }),
+    ])
+  )
+);
