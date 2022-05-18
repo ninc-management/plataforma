a0e321597d1b5d8a4b623ccd34876169deac20b1
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2022-05-18 23:42:30 +0000

    [NWS-1311][NWS-1315] Separate another functions from report to separate files

--- src/app/shared/report-generator.ts
+++ src/app/shared/report-generator.ts
@@ -1,7 +1,14 @@
 import { ElementRef } from '@angular/core';
-import { Contract, ContractExpense } from '@models/contract';
 import { format } from 'date-fns';
 import { BrMaskDirective } from './directives/br-mask.directive';
+import { Contract, ContractExpense } from '@models/contract';
+import { Sector } from '@models/shared';
+import { User } from '@models/user';
+
+enum GROUP_BY {
+  USER = 'Usuário',
+  SECTOR = 'Setor',
+}
 
 interface ExpensesData {
   expenses: ContractExpense[];
@@ -47,6 +54,72 @@ export function generateExpensesReport(contract: Contract): string {
     csv += '\r\n';
   });
 
+  return csv;
+}
+
+export function generateUsersReport(
+  data: any,
+  groupBy: GROUP_BY,
+  userRevival: (id: string | User) => User,
+  sectorRevival: (id: string | Sector | undefined) => string
+): string {
+  const header = [
+    '',
+    'Janeiro;;;;',
+    'Fevereiro;;;;',
+    'Março;;;;',
+    'Abril;;;;',
+    'Maio;;;;',
+    'Junho;;;;',
+    'Julho;;;;',
+    'Agosto;;;;',
+    'Setembro;;;;',
+    'Outubro;;;;',
+    'Novembro;;;;',
+    'Dezembro;;;;',
+    'Resumos;;;;;;;;',
+  ];
+  const monthlySubHeader = ['Recebido', 'Despesas', 'Orçamentos Passados', 'Contratos Fechado', 'Contrato Finalizados'];
+  const overviewSubHeader = [
+    'Total Recebido',
+    'Total Despesas',
+    'A Receber',
+    'Total Orçamentos Gestor',
+    'Total Orçamentos Equipe',
+    'Total Contratos Gestor',
+    'Total Contratos Equipe',
+    'Total Contrato Finalizados Gestor',
+    'Total Contrato Finalizados Equipe',
+  ];
+  let csv = header.join(';');
+  csv += '\r\n';
+  csv += groupBy == GROUP_BY.USER ? 'Associados;' : 'Setores;';
+  for (let i = 0; i < 12; i++) {
+    csv += monthlySubHeader.join(';') + ';';
+  }
+  csv += overviewSubHeader.join(';');
+  csv += '\r\n';
+
+  for (const key of Object.keys(data)) {
+    csv += (groupBy == GROUP_BY.USER ? idToProperty(key, userRevival, 'fullName') : sectorRevival(key)) + ';';
+    data[key].monthly_data.forEach((individualData: any) => {
+      csv += individualData.received + ';';
+      csv += individualData.expenses + ';';
+      csv += (individualData.sent_invoices_manager + individualData.sent_invoices_team).toString() + ';';
+      csv += (individualData.opened_contracts_manager + individualData.opened_contracts_team).toString() + ';';
+      csv += (individualData.concluded_contracts_manager + individualData.concluded_contracts_team).toString() + ';';
+    });
+    csv += data[key].overview.received + ';';
+    csv += data[key].overview.expenses + ';';
+    csv += data[key].overview.to_receive + ';';
+    csv += data[key].overview.sent_invoices_manager.toString() + ';';
+    csv += data[key].overview.sent_invoices_team.toString() + ';';
+    csv += data[key].overview.opened_contracts_manager.toString() + ';';
+    csv += data[key].overview.opened_contracts_team.toString() + ';';
+    csv += data[key].overview.concluded_contracts_manager.toString() + ';';
+    csv += data[key].overview.concluded_contracts_team.toString();
+    csv += '\r\n';
+  }
   return csv;
 }
 
@@ -110,8 +183,18 @@ function sumMoney(value1: string, value2: string, decimals = 2): string {
   return numberToMoney(moneyToNumber(value1) + moneyToNumber(value2), decimals);
 }
 
+//TODO: trocar depois do commit do eduardo
 function formatDate(date: Date, divider = '/'): string {
   return format(date, 'dd' + divider + 'MM' + divider + 'yyyy');
+}
+
+function idToProperty<F extends (...arg: any) => ReturnType<F>>(
+  id: Parameters<F>[0] | undefined,
+  revival: F,
+  property: keyof ReturnType<F>
+): any {
+  if (id) return revival(id)[property];
+  return '';
 }
 
 function codeSort(direction: number, a: string, b: string): number {
