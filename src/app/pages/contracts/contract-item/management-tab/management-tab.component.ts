172f0848b3b44a3b98a2438de56ac4bb93427676
Leo Holanda <lulh@ic.ufal.br>
2022-05-23 23:22:47 +0000

    [NWS-1409][NWS-1467] Fix percentual item progress

--- src/app/pages/contracts/contract-item/management-tab/management-tab.component.ts
+++ src/app/pages/contracts/contract-item/management-tab/management-tab.component.ts
@@ -204,16 +204,12 @@ export class ManagementTabComponent implements OnInit, OnDestroy {
   }
 
   percentualItemProgress(item: ContractChecklistItem): number {
-    if (item.actionList.length > 0) {
-      const today = new Date();
-      if (item.range.end && isAfter(today, item.range.end)) return 100;
+    if (item.actionList.length == 0) return item.actionList.length;
 
-      const completedActionsQtd = item.actionList.reduce((count, action) => (action.isFinished ? count + 1 : count), 0);
-      return this.stringUtils.moneyToNumber(
-        this.stringUtils.toPercentageNumber(completedActionsQtd, item.actionList.length).slice(0, -1)
-      );
-    }
-    return 0;
+    const completedActionsQtd = item.actionList.reduce((count, action) => (action.isFinished ? count + 1 : count), 0);
+    return this.stringUtils.moneyToNumber(
+      this.stringUtils.toPercentageNumber(completedActionsQtd, item.actionList.length).slice(0, -1)
+    );
   }
 
   hasItemFinished(item: ContractChecklistItem): boolean {
