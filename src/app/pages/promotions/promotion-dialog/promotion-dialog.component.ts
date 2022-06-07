5c13496e71027bb3d01ddec4521309c664f0dd74
Elygledson Js <ejsb2@ic.ufal.br>
2022-06-07 23:55:17 +0000

    [NWS-1351][NWS-1532] Add dialog to confirm discarding changes when pressing ESC - Promotion

--- src/app/pages/promotions/promotion-dialog/promotion-dialog.component.ts
+++ src/app/pages/promotions/promotion-dialog/promotion-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input, Inject, Optional } from '@angular/core';
-import { NbDialogRef, NB_DOCUMENT } from '@nebular/theme';
+import { NbDialogRef, NbDialogService, NB_DOCUMENT } from '@nebular/theme';
 import { BaseDialogComponent } from 'app/shared/components/base-dialog/base-dialog.component';
 import { Promotion } from '@models/promotion';
 import { isPhone, tooltipTriggers } from 'app/shared/utils';
+import { ConfirmationDialogComponent } from 'app/shared/components/confirmation-dialog/confirmation-dialog.component';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-promotion-dialog',
@@ -18,12 +20,36 @@ export class PromotionDialogComponent extends BaseDialogComponent implements OnI
 
   constructor(
     @Inject(NB_DOCUMENT) protected derivedDocument: Document,
-    @Optional() protected derivedRef: NbDialogRef<PromotionDialogComponent>
+    @Optional() protected derivedRef: NbDialogRef<PromotionDialogComponent>,
+    private dialogService: NbDialogService
   ) {
     super(derivedDocument, derivedRef);
   }
 
   ngOnInit(): void {
     super.ngOnInit();
+  }
+
+  dismiss(): void {
+    if (this.isFormDirty.value) {
+      this.dialogService
+        .open(ConfirmationDialogComponent, {
+          context: {
+            question: 'Deseja descartar as alterações feitas ?',
+          },
+          dialogClass: 'my-dialog',
+          closeOnBackdropClick: false,
+          closeOnEsc: false,
+          autoFocus: false,
+        })
+        .onClose.pipe(take(1))
+        .subscribe((response: boolean) => {
+          if (response) {
+            super.dismiss();
+          }
+        });
+    } else {
+      super.dismiss();
+    }
   }
 }
