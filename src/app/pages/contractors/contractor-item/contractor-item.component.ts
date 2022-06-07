3adf6e3897fc4923234318bb797692ada641cdb9
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2022-06-07 23:55:01 +0000

    Display confirmation dialog to dismiss data from contractor

--- src/app/pages/contractors/contractor-item/contractor-item.component.ts
+++ src/app/pages/contractors/contractor-item/contractor-item.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { cloneDeep } from 'lodash';
 import { ContractorService } from 'app/shared/services/contractor.service';
 import { Contractor } from '@models/contractor';
 import contractor_validation from 'app/shared/contractor-validation.json';
+import { BehaviorSubject } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'ngx-contractor-item',
@@ -11,7 +13,9 @@ import contractor_validation from 'app/shared/contractor-validation.json';
 })
 export class ContractorItemComponent implements OnInit {
   @Input() iContractor = new Contractor();
+  @Input() isFormDirty = new BehaviorSubject<boolean>(false);
   @Output() submit = new EventEmitter<void>();
+  @ViewChild('form') ngForm = {} as NgForm;
   contractor = new Contractor();
   editing = false;
   submitted = false;
@@ -26,10 +30,17 @@ export class ContractorItemComponent implements OnInit {
     }
   }
 
+  ngAfterViewInit() {
+    this.ngForm.statusChanges?.subscribe(() => {
+      if (this.ngForm.dirty) this.isFormDirty.next(true);
+    });
+  }
+
   registerContractor(): void {
     this.submitted = true;
     if (this.editing) this.contractorService.editContractor(this.contractor);
     else this.contractorService.saveContractor(this.contractor);
+    this.isFormDirty.next(false);
     this.submit.emit();
   }
 }
