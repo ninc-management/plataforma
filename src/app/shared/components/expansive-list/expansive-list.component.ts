a056de3ac6fdd20a26d5ab08fd275d80f613e252
Leo Holanda <lulh@ic.ufal.br>
2022-06-08 20:05:25 +0000

    [NWS-784][NWS-1534] Improve expansive list component

--- src/app/shared/components/expansive-list/expansive-list.component.ts
+++ src/app/shared/components/expansive-list/expansive-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { trackByIndex } from 'app/shared/utils';
 
 @Component({
@@ -7,35 +7,45 @@ import { trackByIndex } from 'app/shared/utils';
   styleUrls: ['./expansive-list.component.scss'],
 })
 export class ExpansiveListComponent implements OnInit, AfterViewInit {
+  @ViewChild('list', { read: ElementRef }) list!: ElementRef;
   @ViewChild('listItem', { read: ElementRef }) listItem!: ElementRef;
   @ViewChild('cardHeader', { read: ElementRef }) cardHeader!: ElementRef;
   @Input() title = '';
   @Input() itens: string[] = [];
-  @Input() min?: number;
+  @Input() min?: number = 5;
   @Input() max?: number;
 
+  minHeight = 0;
   maxHeight = 0;
+  hasOverflow = false;
+  hasUnderflow = false;
   isExpanded = false;
 
   trackByIndex = trackByIndex;
 
   constructor() {}
 
-  //se os itens não passarem do limite de height, não precisa mostrar o botão de mostrar mais
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.max && this.min) {
+      this.max = this.min * 2;
+      if (this.itens.length > this.max) this.hasOverflow = true;
+      if (this.itens.length < this.min) this.hasUnderflow = true;
+    }
+  }
 
   ngAfterViewInit(): void {
-    const cardOffset = 2;
+    const cardOffset = 4;
 
     setTimeout(() => {
       const cardHeaderHeight = this.cardHeader.nativeElement.offsetHeight;
       const listItemHeight = this.listItem.nativeElement.offsetHeight;
-      if (this.min) this.maxHeight = cardHeaderHeight - cardOffset + listItemHeight * this.min;
-      else this.maxHeight = cardHeaderHeight - cardOffset + listItemHeight * this.itens.length;
+      if (this.min) this.minHeight = cardHeaderHeight - cardOffset + listItemHeight * this.min;
+      if (this.max) this.maxHeight = cardHeaderHeight - cardOffset + listItemHeight * this.max;
     }, 10);
   }
 
   toggleExpansive(): void {
+    if (this.hasOverflow && this.isExpanded) this.list.nativeElement.scrollTop = 0;
     this.isExpanded = !this.isExpanded;
   }
 }
