72ffa7e2c4ae9191a8004392604fed81ef157d2a
Leo Holanda <lulh@ic.ufal.br>
2022-07-04 18:25:06 +0000

    Improve expansive grid component

--- src/app/pages/dashboard/expansive-grid/expansive-grid.component.ts
+++ src/app/pages/dashboard/expansive-grid/expansive-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { NbCardBodyComponent } from '@nebular/theme';
 
 import { MetricItem } from '../metric-item/metric-item.component';
 import { trackByIndex } from 'app/shared/utils';
@@ -9,11 +10,11 @@ import { trackByIndex } from 'app/shared/utils';
   styleUrls: ['./expansive-grid.component.scss'],
 })
 export class ExpansiveGridComponent implements OnInit {
-  @ViewChild('list', { read: ElementRef }) list!: ElementRef;
+  @ViewChild(NbCardBodyComponent, { read: ElementRef }) cardBody!: ElementRef;
   @Input() title = '';
   @Input() items: MetricItem[] = [];
-  @Input() min?: number = 5;
-  @Input() max?: number;
+  @Input() minRows?: number = 2;
+  @Input() maxRows?: number;
   @Input() isLoading: boolean = false;
 
   minHeight = 0;
@@ -25,20 +26,25 @@ export class ExpansiveGridComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    if (!this.max && this.min) this.max = this.min * 2;
+    if (!this.maxRows && this.minRows) this.maxRows = this.minRows * 2;
 
     //All values are used as rem
     const borderWitdh = 0.063;
+    const cardBodyPadding = 1;
+    const gridMargin = 1;
+    const gridItemHeight = 7;
+    const gridGap = 1;
+    const rowHeight = gridItemHeight + gridGap;
+
     //Padding (top + bottom) + line height + border for both constants
     const cardHeaderHeight = 2 + 1.5 + borderWitdh;
-    const listItemHeight = 2 + 1.25 + borderWitdh;
-    //It's necessary to sum 1 borderWitdh because the first list item has a top border
-    if (this.min) this.minHeight = cardHeaderHeight + borderWitdh + listItemHeight * this.min;
-    if (this.max) this.maxHeight = cardHeaderHeight + borderWitdh + listItemHeight * this.max;
+
+    if (this.minRows) this.minHeight = cardHeaderHeight + cardBodyPadding + gridMargin + rowHeight * this.minRows;
+    if (this.maxRows) this.maxHeight = cardHeaderHeight + cardBodyPadding + gridMargin + rowHeight * this.maxRows;
   }
 
   toggleExpansive(): void {
-    if (this.max && this.items.length > this.max && this.isExpanded) this.list.nativeElement.scrollTop = 0;
+    if (this.maxRows && this.items.length > this.maxRows && this.isExpanded) this.cardBody.nativeElement.scrollTop = 0;
     this.isExpanded = !this.isExpanded;
   }
 }
