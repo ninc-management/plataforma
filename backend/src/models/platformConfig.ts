816ac0a2ace1c84e2131560aaf41e88374e041ce
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2022-07-05 20:55:32 -0300

    [NWS-1378][NWS-1641] Add company name display button
    * [NWS-1378][NWS-1641] Add company name display button
    
    * CR fixes
    
    * Correcting name, id according to input name
    
    * CR fixes

--- backend/src/models/platformConfig.ts
+++ backend/src/models/platformConfig.ts
@@ -89,6 +89,9 @@ export class SocialConfig {
   companyName: string = '';
 
   @prop({ required: true })
+  showCompanyName: boolean = true;
+
+  @prop({ required: true })
   address: string = '';
 
   @prop({ required: true })
