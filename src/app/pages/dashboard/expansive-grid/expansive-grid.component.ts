472ddfab2c60b38868928657f9cc63bcfcb17f22
Leo Holanda <lulh@ic.ufal.br>
2022-07-05 23:30:56 +0000

    Fix input restrains in expansive list and grid

--- src/app/pages/dashboard/expansive-grid/expansive-grid.component.ts
+++ src/app/pages/dashboard/expansive-grid/expansive-grid.component.ts
@@ -13,8 +13,8 @@ export class ExpansiveGridComponent implements OnInit {
   @ViewChild(NbCardBodyComponent, { read: ElementRef }) cardBody!: ElementRef;
   @Input() title = '';
   @Input() items: MetricItem[] = [];
-  @Input() minRows?: number = 2;
-  @Input() maxRows?: number;
+  @Input() minRows: number = 2;
+  @Input() maxRows!: number;
   @Input() isLoading: boolean = false;
 
   minHeight = 0;
@@ -26,7 +26,7 @@ export class ExpansiveGridComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    if (!this.maxRows && this.minRows) this.maxRows = this.minRows * 2;
+    if (!this.maxRows) this.maxRows = this.minRows * 2;
 
     //All values are used as rem
     const borderWitdh = 0.063;
@@ -39,12 +39,12 @@ export class ExpansiveGridComponent implements OnInit {
     //Padding (top + bottom) + line height + border for both constants
     const cardHeaderHeight = 2 + 1.5 + borderWitdh;
 
-    if (this.minRows) this.minHeight = cardHeaderHeight + cardBodyPadding + gridMargin + rowHeight * this.minRows;
-    if (this.maxRows) this.maxHeight = cardHeaderHeight + cardBodyPadding + gridMargin + rowHeight * this.maxRows;
+    this.minHeight = cardHeaderHeight + cardBodyPadding + gridMargin + rowHeight * this.minRows;
+    this.maxHeight = cardHeaderHeight + cardBodyPadding + gridMargin + rowHeight * this.maxRows;
   }
 
   toggleExpansive(): void {
-    if (this.maxRows && this.items.length > this.maxRows && this.isExpanded) this.cardBody.nativeElement.scrollTop = 0;
+    if (this.items.length > this.maxRows && this.isExpanded) this.cardBody.nativeElement.scrollTop = 0;
     this.isExpanded = !this.isExpanded;
   }
 }
