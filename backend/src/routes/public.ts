43b213fe36df3cbef26c19fae2af5b2a69dea65a
Elygledson Js <ejsb2@ic.ufal.br>
2022-08-29 22:34:17 +0000

    Check and Fix Team Page

--- backend/src/routes/public.ts
+++ backend/src/routes/public.ts
@@ -1,13 +1,16 @@
 import * as express from 'express';
+import { isEqual } from 'lodash';
 
 import ContractModel, { Contract } from '../models/contract';
 import ContractorModel, { Contractor } from '../models/contractor';
+import TeamModel, { Team } from '../models/team';
 import UserModel, { User } from '../models/user';
 
 const router = express.Router();
 
 router.post('/user/all', async (req, res) => {
   const users: User[] = await UserModel.find({});
+  const teams: Team[] = await TeamModel.find({});
   return res.status(200).json(
     users
       .filter((user) => user.active)
@@ -15,6 +18,13 @@ router.post('/user/all', async (req, res) => {
         fullName: user.fullName,
         profilePicture: user.profilePicture,
         exibitionName: user.exibitionName,
+        teamsAsMember: teams
+          .filter((team) => {
+            return team.members.some((member) => {
+              return isEqual(member.user, user._id);
+            });
+          })
+          .map((team) => team.name),
       }))
   );
 });
