fdc07b001083bc84c6c2dd41d517040633b28a3d
Augusto Ícaro Farias da Cunha <contact@augustoicaro.com>
2022-11-29 16:04:02 -0800

    Update profile picture string to variable

--- src/app/shared/services/invoice.service.spec.ts
+++ src/app/shared/services/invoice.service.spec.ts
@@ -289,9 +289,9 @@ describe('InvoiceService', () => {
 
   baseTest('idToProfilePicture should work', (expectedInvoices: Invoice[]) => {
     expect(service.idToProfilePicture(undefined)).toEqual('');
-    expect(service.idToProfilePicture('0')).toEqual('pic0@pic.com');
+    expect(service.idToProfilePicture('0')).toEqual(externalMockedUsers[0].profilePicture);
     expect(service.idToProfilePicture(mockedInvoices[0])).toEqual((expectedInvoices[0].author as User).profilePicture!);
-    expect(service.idToProfilePicture('1')).toEqual('pic1@pic.com');
+    expect(service.idToProfilePicture('1')).toEqual(externalMockedUsers[1].profilePicture);
     expect(service.idToProfilePicture(mockedInvoices[1])).toEqual((expectedInvoices[1].author as User).profilePicture!);
   });
 
