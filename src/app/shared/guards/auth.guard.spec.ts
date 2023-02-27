80957aedaa3f46e9b2ec6a243ab917452a43d080
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-02-27 20:41:23 -0300

    [NINC-1902][NINC-1909] Make interceptor to pass the logged in user's company ID on all requests
    * Make interceptor to pass the logged in user's company ID on all requests
    
    * CR fixes

--- src/app/shared/guards/auth.guard.spec.ts
+++ src/app/shared/guards/auth.guard.spec.ts
@@ -9,35 +9,42 @@ import { CommonTestingModule } from 'app/../common-testing.module';
 import { cloneDeep } from 'lodash';
 import { Observable, of, take } from 'rxjs';
 
-import { ConfigService, DEFAULT_CONFIG } from '../services/config.service';
+import { externalMockedConfigs } from '../mocked-data/mocked-config';
+import { externalMockedUsers } from '../mocked-data/mocked-users';
+import { ConfigService } from '../services/config.service';
 import { AuthGuard } from './auth.guard';
 
-import { PlatformConfig, ProfileConfig } from '@models/platformConfig';
-import { ExpenseType } from '@models/team';
+import { PlatformConfig } from '@models/platformConfig';
+import { User } from '@models/user';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
   let router: jasmine.SpyObj<Router>;
   let httpMock: HttpTestingController;
   let configService: ConfigService;
+  let mockedUsers: User[];
   let mockedConfigs: PlatformConfig[];
   const next = new ActivatedRouteSnapshot();
   const childRoute = new ActivatedRouteSnapshot();
-  const authServiceSpy = jasmine.createSpyObj<NbAuthService>('NbAuthService', ['isAuthenticated']);
+  const nbAuthServiceSpy = jasmine.createSpyObj<NbAuthService>('NbAuthService', ['isAuthenticated']);
   const nbAclServiceSpy = jasmine.createSpyObj<NbAclService>('NbAclService', ['setAccessControl']);
   const nbAccessCheckerSpy = jasmine.createSpyObj<NbAccessChecker>('NbAccessChecker', ['isGranted']);
-  const instanceSpy = jasmine.createSpyObj<IPublicClientApplication>('IPublicClientApplication', ['getAllAccounts']);
+  const instanceSpy = jasmine.createSpyObj<IPublicClientApplication>('IPublicClientApplication', [
+    'getAllAccounts',
+    'getActiveAccount',
+    'setActiveAccount',
+    'loginPopup',
+  ]);
   const msAuthServiceSpy = jasmine.createSpyObj<MsalService>('MsalService', [], {
     instance: instanceSpy,
   });
 
-  CommonTestingModule.setUpTestBed();
   const stateSpy = jasmine.createSpyObj('RouterStateSnapshot', ['toString']);
 
   CommonTestingModule.setUpTestBed();
 
   beforeEach(() => {
-    TestBed.overrideProvider(NbAuthService, { useValue: authServiceSpy });
+    TestBed.overrideProvider(NbAuthService, { useValue: nbAuthServiceSpy });
     TestBed.overrideProvider(NbAclService, { useValue: nbAclServiceSpy });
     TestBed.overrideProvider(MsalService, { useValue: msAuthServiceSpy });
     TestBed.overrideProvider(NbAccessChecker, { useValue: nbAccessCheckerSpy });
@@ -46,54 +53,64 @@ describe('AuthGuard', () => {
     router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
     httpMock = TestBed.inject(HttpTestingController);
     configService = TestBed.inject(ConfigService);
-    mockedConfigs = [];
-    const mockedConfig = cloneDeep(DEFAULT_CONFIG) as any;
-    mockedConfig._id = '0';
-    mockedConfig.profileConfig.levels.push('teste');
-    mockedConfig.profileConfig.positions = [
-      { roleTypeName: 'teste', permission: 'Administrador' },
-      { roleTypeName: 'teste2', permission: 'Membro' },
-      { roleTypeName: 'teste3', permission: 'Financeiro' },
-    ];
-    mockedConfigs.push(cloneDeep(mockedConfig));
+    mockedConfigs = cloneDeep(externalMockedConfigs) as any;
+    mockedUsers = cloneDeep(externalMockedUsers);
     configService.getConfig().pipe(take(1)).subscribe();
     const req = httpMock.expectOne('/api/config/all');
     expect(req.request.method).toBe('POST');
     req.flush(mockedConfigs);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
 
-  it('should be not athenticated and redirected to auth/login', (done: DoneFn) => {
-    authServiceSpy.isAuthenticated.and.returnValue(of(false));
+  it('should be not authenticated and redirected to auth/login', (done: DoneFn) => {
+    nbAuthServiceSpy.isAuthenticated.and.returnValue(of(false));
     instanceSpy.getAllAccounts.and.returnValue([]);
     spyOn(router, 'navigate');
     (guard.canActivate(next, stateSpy) as Observable<boolean>).subscribe((result) => {
-      expect(result).toBe(false, 'user is authenticated');
+      expect(result).toBe(false, 'user is not authenticated');
       expect(router.navigate).toHaveBeenCalledWith(['auth/login']);
       done();
     });
   });
 
-  it('should be athenticated and not redirected to auth/login', (done: DoneFn) => {
-    authServiceSpy.isAuthenticated.and.returnValue(of(true));
+  it('should be authenticated and not redirected to auth/login', (done: DoneFn) => {
+    nbAuthServiceSpy.isAuthenticated.and.returnValue(of(true));
     instanceSpy.getAllAccounts.and.returnValue([
       {
-        homeAccountId: 'test',
-        environment: 'test',
-        tenantId: 'test',
-        username: 'test',
-        localAccountId: 'test',
+        homeAccountId: mockedUsers[0]._id,
+        environment: '',
+        tenantId: mockedUsers[0]._id,
+        name: mockedUsers[0].fullName,
+        username: mockedUsers[0].email,
+        localAccountId: mockedUsers[0]._id,
       },
     ]);
+    instanceSpy.getActiveAccount.and.returnValue({
+      homeAccountId: mockedUsers[0]._id,
+      environment: '',
+      tenantId: mockedUsers[0]._id,
+      name: mockedUsers[0].fullName,
+      username: mockedUsers[0].email,
+      localAccountId: mockedUsers[0]._id,
+    });
     spyOn(router, 'navigate');
     (guard.canActivate(next, stateSpy) as Observable<boolean>).subscribe((result) => {
-      expect(result).toBe(true, 'user is not authenticated');
+      expect(result).toBe(true, 'user is authenticated');
       expect(router.navigate).not.toHaveBeenCalled();
       done();
     });
+    const req = httpMock.expectOne('/api/auth/id');
+    expect(req.request.method).toBe('POST');
+    setTimeout(() => {
+      req.flush(mockedUsers[0].company ? mockedUsers[0].company : '');
+    }, 50);
   });
 
   it('should allow navigate to childRoute', (done: DoneFn) => {
@@ -120,9 +137,9 @@ describe('AuthGuard', () => {
 
   it('loadList should work', (done: DoneFn) => {
     const accessControl = {
-      teste: { parent: 'Diretor de T.I' },
-      teste2: { parent: 'Associado' },
-      teste3: { parent: 'Diretor Financeiro' },
+      Administrador: { parent: 'Diretor de T.I' },
+      Membro: { parent: 'Associado' },
+      Financeiro: { parent: 'Diretor Financeiro' },
     };
     guard
       .loadList()
