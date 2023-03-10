dfee1c62e1889d30faf6e9d1b465768897a060cd
Augsuto Ícaro Farias da Cunha <contact@augustoicaro.com>
2023-03-09 21:41:46 -0800

    [NINC-2021][NINC-2052] Add page break to invoice's ads page

--- src/app/pages/invoices/pdf.service.ts
+++ src/app/pages/invoices/pdf.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import extenso from 'extenso';
-import { Img, PdfMakeWrapper } from 'pdfmake-wrapper';
 import pdfMake from 'pdfmake/build/pdfmake';
 import { ContentTable, ContextPageSize, TableCell } from 'pdfmake/interfaces';
+import { Img, PdfMakeWrapper } from 'pdfmake-wrapper';
 import { Subject, take } from 'rxjs';
 
 import { CompanyService } from 'app/shared/services/company.service';
@@ -1452,6 +1452,7 @@ export class PdfService {
         },
       ],
       margin: [5, 20, 5, 5],
+      pageBreak: 'before',
     });
 
     pdf.add({
