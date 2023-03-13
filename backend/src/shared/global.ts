519b9931f00e08ecc60e7decd422e55f46b2ed52
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-03-13 20:20:56 -0300

    [NINC-1902][NINC-2034] Add company DB handling to the team route
    * Add company DB handling to the team route
    
    * CR fixes

--- backend/src/shared/global.ts
+++ backend/src/shared/global.ts
@@ -2,18 +2,15 @@ import { mongoose } from '@typegoose/typegoose';
 import { Subject } from 'rxjs';
 
 import { Company } from '../models/company';
-import { Contractor } from '../models/contractor';
 import { Course } from '../models/course';
 import { Notification } from '../models/notification';
 import { PlatformConfig } from '../models/platformConfig';
 import { Prospect } from '../models/prospect';
 import { Provider } from '../models/provider';
-import { Team } from '../models/team';
 import { User } from '../models/user';
 
 export const usersMap: Record<string, User> = {};
 export const prospectMap: Record<string, Prospect> = {};
-export const teamMap: Record<string, Team> = {};
 export const configMap: Record<string, PlatformConfig> = {};
 export const coursesMap: Record<string, Course> = {};
 export const notification$ = new Subject<Notification>();
