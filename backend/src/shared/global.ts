8d37175a904d84e907815a389a41f0d9c5972415
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-03-15 17:28:37 -0300

    [NINC-1902][NINC-2035] Add company DB handling to the course route
    Co-authored-by: Augusto Ícaro Farias da Cunha <contact@augustoicaro.com>

--- backend/src/shared/global.ts
+++ backend/src/shared/global.ts
@@ -2,7 +2,6 @@ import { mongoose } from '@typegoose/typegoose';
 import { Subject } from 'rxjs';
 
 import { Company } from '../models/company';
-import { Course } from '../models/course';
 import { Notification } from '../models/notification';
 import { PlatformConfig } from '../models/platformConfig';
 import { Prospect } from '../models/prospect';
@@ -12,7 +11,6 @@ import { User } from '../models/user';
 export const usersMap: Record<string, User> = {};
 export const prospectMap: Record<string, Prospect> = {};
 export const configMap: Record<string, PlatformConfig> = {};
-export const coursesMap: Record<string, Course> = {};
 export const notification$ = new Subject<Notification>();
 export const providersMap: Record<string, Provider> = {};
 export const companyMap: Record<string, Company> = {};
