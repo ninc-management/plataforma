7299d8975dc580240e7277db325b4ac5edab0e2d
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-04-17 18:25:20 -0300

    [NINC-1902][NINC-2040] Add company DB handling to the company route
    * Add company DB handling to the company route
    
    * CR fixes

--- backend/src/routes/company.ts
+++ backend/src/routes/company.ts
@@ -1,55 +1,41 @@
-import { Mutex } from 'async-mutex';
 import * as express from 'express';
-import { cloneDeep } from 'lodash';
 
 import CompanyModel, { Company } from '../models/company';
-import { companyMap } from '../shared/global';
 
 const router = express.Router();
-let requested = false;
-const mutex = new Mutex();
 
 router.post('/', async (req, res, next) => {
   const company = new CompanyModel(req.body.company);
-  mutex.acquire().then((release) => {
-    company
-      .save()
-      .then((savedCompany) => {
-        if (requested) companyMap[savedCompany._id] = cloneDeep(savedCompany.toJSON());
-        release();
-        return res.status(201).json({
-          message: 'Empresa cadastrada!',
-        });
-      })
-      .catch((err) => {
-        release();
-        return res.status(500).json({
-          message: 'Erro ao cadastrar empresa!',
-          error: err,
-        });
-      });
-  });
+  try {
+    await company.save();
+    return res.status(201).json({
+      message: 'Empresa cadastrada!',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao cadastrar empresa!',
+      error: err,
+    });
+  }
 });
 
 router.post('/all', async (req, res) => {
-  if (!requested) {
+  try {
     const companies: Company[] = await CompanyModel.find({});
-    companies.map((company) => (companyMap[company._id] = cloneDeep(company)));
-    requested = true;
+    return res.status(200).json(companies);
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao buscar empresas!',
+      error: err,
+    });
   }
-  return res.status(200).json(Array.from(Object.values(companyMap)));
 });
 
 router.post('/update', async (req, res, next) => {
   try {
-    const savedCompany = await CompanyModel.findByIdAndUpdate(req.body.company._id, req.body.company, {
+    await CompanyModel.findByIdAndUpdate(req.body.company._id, req.body.company, {
       upsert: false,
     });
-    if (requested) {
-      await mutex.runExclusive(async () => {
-        companyMap[req.body.company._id] = cloneDeep(savedCompany.toJSON());
-      });
-    }
     return res.status(200).json({
       message: 'Dados da empresa atualizados!',
     });
--- backend/src/shared/global.ts
+++ backend/src/shared/global.ts
@@ -1,7 +1,6 @@
 import { mongoose } from '@typegoose/typegoose';
 import { Subject } from 'rxjs';
 
-import { Company } from '../models/company';
 import { Notification } from '../models/notification';
 import { PlatformConfig } from '../models/platformConfig';
 import { Prospect } from '../models/prospect';
@@ -11,5 +10,4 @@ export const usersMap: Record<string, User> = {};
 export const prospectMap: Record<string, Prospect> = {};
 export const configMap: Record<string, PlatformConfig> = {};
 export const notification$ = new Subject<Notification>();
-export const companyMap: Record<string, Company> = {};
 export const connectionPool: Record<string, mongoose.Connection> = {};
