82195cf2cda41571fa37e6e631801545b49278bd
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-04-17 18:25:20 -0300

    [NINC-1902][NINC-2040] Add company DB handling to the company route
    * Add company DB handling to the company route
    
    * CR fixes

--- backend/src/routes/company.ts
+++ backend/src/routes/company.ts
@@ -1,43 +1,34 @@
-import { Mutex } from 'async-mutex';
 import * as express from 'express';
-import { cloneDeep } from 'lodash';
 
 import CompanyModel, { Company } from '../models/company';
-import { companyMap } from '../shared/global';
 
 const router = express.Router();
-let requested = false;
-const mutex = new Mutex();
 
 router.post('/', async (req, res, next) => {
   const company = new CompanyModel(req.body.company);
-  mutex.acquire().then((release) => {
-    company
-      .save()
-      .then((savedCompany) => {
-        if (requested) companyMap[savedCompany._id] = cloneDeep(savedCompany.toJSON());
-        release();
-        return res.status(201).json({
-          message: 'Empresa cadastrada!',
-        });
-      })
-      .catch((err) => {
-        release();
-        return res.status(500).json({
-          message: 'Erro ao cadastrar empresa!',
-          error: err,
-        });
-      });
-  });
+  try {
+    await company.save();
+    return res.status(201).json({
+      message: 'Empresa cadastrada!',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao cadastrar empresa!',
+      error: err,
+    });
+  }
 });
 
 router.post('/all', async (req, res) => {
-  if (!requested) {
+  try {
     const companies: Company[] = await CompanyModel.find({});
-    companies.map((company) => (companyMap[company._id] = cloneDeep(company)));
-    requested = true;
+    return res.status(200).json(companies);
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao buscar empresas!',
+      error: err,
+    });
   }
-  return res.status(200).json(Array.from(Object.values(companyMap)));
 });
 
 router.post('/update', async (req, res, next) => {
@@ -50,11 +41,6 @@ router.post('/update', async (req, res, next) => {
     if (!company) {
       return res.status(500).json({
         message: 'O documento foi atualizado por outro usuário. Por favor, recarregue os dados e tente novamente.',
-      });
-    }
-    if (requested) {
-      await mutex.runExclusive(async () => {
-        companyMap[req.body.company._id] = cloneDeep(company.toJSON());
       });
     }
     return res.status(200).json({
