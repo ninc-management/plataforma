7750a43de44e4aed8c1bd6da47f9d2b11c389a6e
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-04-17 18:30:46 -0300

    [NINC-1902][NINC-2023] Add company DB handling to the user route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,46 +1,33 @@
-import { Mutex } from 'async-mutex';
 import * as express from 'express';
-import { cloneDeep } from 'lodash';
 
 import ProspectModel, { Prospect } from '../models/prospect';
 import UserModel, { User } from '../models/user';
-import { prospectMap, usersMap } from '../shared/global';
+import { getModelForCompany } from '../shared/util';
 
 const router = express.Router();
-let requested = false;
-let prospectRequested = false;
-const mutex = new Mutex();
 
-router.post('/', (req, res, next) => {
-  const user = new UserModel(req.body.user);
-  mutex.acquire().then((release) => {
-    user
-      .save()
-      .then((savedUser) => {
-        if (requested) usersMap[savedUser._id] = cloneDeep(savedUser.toJSON());
-        release();
-        return res.status(201).json({
-          message: 'Associado cadastrado!',
-        });
-      })
-      .catch((err) => {
-        release();
-        return res.status(500).json({
-          message: 'Erro ao cadastrar associado!',
-          error: err,
-        });
-      });
-  });
+router.post('/', async (req, res, next) => {
+  try {
+    const companyId = req.headers.companyid as string;
+    const userCompanyModel = await getModelForCompany(companyId, UserModel);
+    const user = new userCompanyModel(req.body.user);
+    await user.save();
+    return res.status(201).json({
+      message: 'Associado cadastrado!',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao cadastrar associado!',
+      error: err,
+    });
+  }
 });
 
 router.post('/update', async (req, res, next) => {
   try {
-    const savedUser = await UserModel.findByIdAndUpdate(req.body.user._id, req.body.user, { upsert: false });
-    if (requested) {
-      await mutex.runExclusive(async () => {
-        usersMap[req.body.user._id] = cloneDeep(savedUser.toJSON());
-      });
-    }
+    const companyId = req.headers.companyid as string;
+    const userCompanyModel = await getModelForCompany(companyId, UserModel);
+    await userCompanyModel.findByIdAndUpdate(req.body.user._id, req.body.user, { upsert: false });
     return res.status(200).json({
       message: 'Associado Atualizado!',
     });
@@ -53,59 +40,50 @@ router.post('/update', async (req, res, next) => {
 });
 
 router.post('/all', async (req, res) => {
-  if (!requested) {
-    const users: User[] = await UserModel.find({});
-    users.map((user) => (usersMap[user._id] = cloneDeep(user)));
-    requested = true;
+  try {
+    const companyId = req.headers.companyid as string;
+    const userCompanyModel = await getModelForCompany(companyId, UserModel);
+    const users: User[] = await userCompanyModel.find({});
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao buscar associados!',
+      error: err,
+    });
   }
-  return res.status(200).json(Array.from(Object.values(usersMap)));
 });
 
 router.post('/allProspects', async (req, res) => {
-  if (!prospectRequested) {
-    const prospects: Prospect[] = await ProspectModel.find({});
-    prospects.forEach((prospect) => (prospectMap[prospect._id] = cloneDeep(prospect)));
-    prospectRequested = true;
+  try {
+    const companyId = req.headers.companyid as string;
+    const prospectCompanyModel = await getModelForCompany(companyId, ProspectModel);
+    const prospects: Prospect[] = await prospectCompanyModel.find({});
+    return res.status(200).json(prospects);
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao buscar prospectos!',
+      error: err,
+    });
   }
-  return res.status(200).json(Array.from(Object.values(prospectMap)));
 });
 
 router.delete('/approveProspect', async (req, res, next) => {
-  ProspectModel.deleteOne({ _id: req.body.prospect._id })
-    .then(() => {
-      delete prospectMap[req.body.prospect._id];
-      const newUser = new UserModel(req.body.prospect);
-      newUser
-        .save()
-        .then((savedUser) => {
-          usersMap[savedUser._id] = cloneDeep(savedUser.toJSON());
-          return res.status(201).json({
-            message: 'Prospecto aprovado com sucesso!',
-          });
-        })
-        .catch((newUserErr) => {
-          ProspectModel.create(req.body.prospect)
-            .then((prospect) => {
-              prospectMap[prospect._id] = cloneDeep(prospect.toJSON());
-              return res.status(500).json({
-                message: 'Erro ao criar novo usuário! Prospecto recriado.',
-                error: newUserErr,
-              });
-            })
-            .catch((prospectErr) => {
-              return res.status(500).json({
-                message: 'Erro ao recriar prospecto!',
-                error: prospectErr,
-              });
-            });
-        });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        message: 'Erro ao apagar o prospecto! Não foi possível aprovar.',
-        error: err,
-      });
+  try {
+    const companyId = req.headers.companyid as string;
+    const userCompanyModel = await getModelForCompany(companyId, UserModel);
+    const prospectCompanyModel = await getModelForCompany(companyId, ProspectModel);
+    const newUser = new userCompanyModel(req.body.prospect);
+    await newUser.save();
+    await prospectCompanyModel.findByIdAndDelete(req.body.prospect._id);
+    return res.status(201).json({
+      message: 'Prospecto aprovado com sucesso!',
     });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao criar novo usuário!',
+      error: err,
+    });
+  }
 });
 
 export default router;
