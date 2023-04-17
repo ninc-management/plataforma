4f7802380a695079b94622e28ba217d6c1577271
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-04-17 18:30:46 -0300

    [NINC-1902][NINC-2023] Add company DB handling to the user route

--- backend/src/shared/global.ts
+++ backend/src/shared/global.ts
@@ -4,9 +4,7 @@ import { Subject } from 'rxjs';
 import { Notification } from '../models/notification';
 import { PlatformConfig } from '../models/platformConfig';
 import { Prospect } from '../models/prospect';
-import { User } from '../models/user';
 
-export const usersMap: Record<string, User> = {};
 export const prospectMap: Record<string, Prospect> = {};
 export const configMap: Record<string, PlatformConfig> = {};
 export const notification$ = new Subject<Notification>();
