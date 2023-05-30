45a85f68ad3284d0fc295225f4ce487735677bb8
Elygledson Js <ejsb2@ic.ufal.br>
2023-05-30 01:43:03 +0000

    add global plugin

---
+++ backend/src/models/shared/versionPlugin.ts
@@ -0,0 +1,17 @@
+import 'core-js/stable';
+
+import 'regenerator-runtime/runtime';
+
+/**
+ * Implement optimistic concurrency control on a Mongoose schema.
+ *
+ * @param {mongoose.Schema} schema - A Mongoose schema to be plugged into.
+ * @param {object} options - A Mongoose schema to be plugged into.
+ */
+
+export function updateIfCurrentPlugin(schema: any, options: any) {
+  schema.pre('findOneAndUpdate', function (next: any) {
+    this._update.__v += 1;
+    next();
+  });
+}
