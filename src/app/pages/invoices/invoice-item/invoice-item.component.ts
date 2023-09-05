40f5ae9fbe20fd44d6c39503d6ae33dc186b8145
Pâmela Medeiros <pms2@ic.ufal.br>
2023-09-05 19:24:44 -0300

    [NINC-719][NINC-2152] Add description of the parcel value

--- src/app/pages/invoices/invoice-item/invoice-item.component.ts
+++ src/app/pages/invoices/invoice-item/invoice-item.component.ts
@@ -685,6 +685,16 @@ export class InvoiceItemComponent implements OnInit, OnDestroy, AfterViewInit {
     return subtractMoney(this.tempInvoice.value, base === 'product' ? this.options.total : this.options.stageTotal);
   }
 
+  parcelValue() {
+    if (this.tempInvoice.stages[0].parcelQuantity) {
+      return this.stringUtil.numberToMoney(
+        this.stringUtil.moneyToNumber(this.tempInvoice.stages[0].value) / this.tempInvoice.stages[0].parcelQuantity
+      );
+    } else {
+      return '';
+    }
+  }
+
   updateMaterialList(): void {
     if (this.tempInvoice.materialListType == '2') {
       this.tempInvoice.materials.forEach((material, index) => {
