2603e2916e694e8835b04c815b9ea9074755f079
Matheus G. Oliveira <matheus.gomes201@gmail.com>
2023-09-12 20:01:29 -0300

    [NINC-2054][NINC-2162] - Created a new component empty to  public client edit
    Co-authored-by: Ayrton O. Ouriques <kharon404@outlook.com>

---
+++ src/app/public/contractor-edit/contractor-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contractor-edit',
+  templateUrl: './contractor-edit.component.html',
+  styleUrls: ['./contractor-edit.component.scss'],
+})
+export class ContractorEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
