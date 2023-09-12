12b4d4e57827354f8238c0c88a1a4ec6de64665f
Matheus G. Oliveira <matheus.gomes201@gmail.com>
2023-09-12 20:01:29 -0300

    [NINC-2054][NINC-2162] - Created a new component empty to  public client edit
    Co-authored-by: Ayrton O. Ouriques <kharon404@outlook.com>

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { NbCardModule, NbIconModule, NbLayoutModule } from '@nebular/theme';
 
 import { NgxCertificateComponent } from './certificate/certificate.component';
+import { ContractorEditComponent } from './contractor-edit/contractor-edit.component';
 import { NgxPublicRoutingModule } from './public-routing.module';
 import { NgxPublicComponent } from './public.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, NbLayoutModule, NbCardModule, NbIconModule, NgxPublicRoutingModule],
-  declarations: [NgxPublicComponent, NgxCertificateComponent],
+  declarations: [NgxPublicComponent, NgxCertificateComponent, ContractorEditComponent],
 })
 export class NgxPublicModule {}
