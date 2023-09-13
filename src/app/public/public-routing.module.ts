d3f1272c9c0b5011c51c7999a0a879c4d41dfa5c
Ayrton Ouriques <kharon404@outlook.com>
2023-09-13 20:00:43 -0300

    [NINC-2054][NINC-2163] Created edit contractor public route
    Co-authored-by: Matheus G. Oliveira <matheus.gomes201@gmail.com>

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NgxCertificateComponent } from './certificate/certificate.component';
+import { ContractorEditComponent } from './contractor-edit/contractor-edit.component';
 import { NgxPublicComponent } from './public.component';
 
 export const routes: Routes = [
@@ -12,6 +13,10 @@ export const routes: Routes = [
       {
         path: 'accomplishments/:courseID/:participantID',
         component: NgxCertificateComponent,
+      },
+      {
+        path: 'client/:clientID',
+        component: ContractorEditComponent,
       },
     ],
   },
