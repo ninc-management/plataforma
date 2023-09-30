b0c598e2005206a0d3044ac2d6cb1a41b34ea9cb
Ayrton Ouriques <kharon404@outlook.com>
2023-09-29 21:04:20 -0300

    CRFIXES

--- src/app/pages/contractors/contractor-item/contractor-item.component.ts
+++ src/app/pages/contractors/contractor-item/contractor-item.component.ts
@@ -150,7 +150,9 @@ export class ContractorItemComponent implements OnInit {
   }
 
   isSameDocument(fieldValue: string): Contractor[] {
-    if (!this.contractors || this.editing) return [];
-    return this.contractors.filter((contractor) => contractor.document === fieldValue);
+    if (!this.contractors) return [];
+    return this.contractors
+      .filter((contractor) => !(this.editing && contractor._id == this.iContractor._id))
+      .filter((contractor) => contractor.document === fieldValue);
   }
 }
