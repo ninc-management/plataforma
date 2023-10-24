2428ed5c4c89404d2e5ec96cc147355f6ceddfb4
Matheus G. Oliveira <matheus.gomes201@gmail.com>
2023-10-24 20:23:19 -0300

    [NINC-1153][NINC-2185] Implement visual feedback to outdated Team object
    Co-authored-by: Ayrton Ouriques <kharon404@outlook.com>

--- backend/src/routes/team.ts
+++ backend/src/routes/team.ts
@@ -48,7 +48,7 @@ router.post('/update', async (req, res, next) => {
       });
       if (!team) {
         return res.status(500).json({
-          message: 'O documento foi atualizado por outro usuário. Por favor, recarregue os dados e tente novamente.',
+          message: 'O documento foi atualizado por outro usuário. Por favor, reabra o documento e tente novamente.',
         });
       }
       if (requested) {
