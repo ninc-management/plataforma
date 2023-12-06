f02db280fbd2a09cd73d61cd221ccb40a31ef575
AyrtonOuriques2 <124637151+AyrtonOuriques2@users.noreply.github.com>
2023-12-05 21:50:46 -0300

    [NINC-785][NINC-2236] Installed tinymce-angular

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -16,6 +16,7 @@ import {
   NbTooltipModule,
   NbUserModule,
 } from '@nebular/theme';
+import { EditorModule } from '@tinymce/tinymce-angular';
 import * as echarts from 'echarts';
 import { NgxEchartsModule } from 'ngx-echarts';
 
@@ -64,6 +65,7 @@ echarts.registerLocale('PT-BR', langPTBR);
     ThemeModule,
     NbSmartTableModule,
     NbStepperModule,
+    EditorModule,
   ],
   declarations: [
     DashboardComponent,
